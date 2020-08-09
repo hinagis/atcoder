534138b35b0011bde2accfc6e595e5f2a25d06c9
hasevent <hasevent@gmail.com>
2020-08-09 20:13:49 +0900

    j is ok

--- src/bin/j.rs
+++ src/bin/j.rs
@@ -1,17 +1,32 @@
-//use proconio::{input, fastout};
 use proconio::input;
 
-//#[fastout]
+fn calc(dp: &mut Vec<Vec<Vec<f64>>>, n: usize, i: usize, j: usize, k: usize) -> f64 {
+    if dp[i][j][k] > 0.0 {
+        dp[i][j][k]
+    } else if i == 0 && j == 0 && k == 0 {
+        0.0
+    } else {
+        let r = n as f64
+            + if i > 0 { calc(dp, n, i - 1, j, k) * (i as f64) } else { 0.0 }
+            + if j > 0 { calc(dp, n, i + 1, j - 1, k) * (j as f64) } else { 0.0 }
+            + if k > 0 { calc(dp, n, i, j + 1, k - 1) * (k as f64) } else { 0.0 };
+        dp[i][j][k] = r / ((i + j + k) as f64);
+        dp[i][j][k]
+    }
+}
+
 fn main() {
     input! {
-        n: i32,
-        d: [(i32, i32); n],
+        n: usize,
     }
-    let r = n;
-    for (a, b) in d {
-        todo!();
+    let mut ijk = [0usize; 3];
+    for _ in 0..n {
+        input! {
+            a: usize,
+        }
+        ijk[a - 1] += 1;
     }
+    let mut dp = vec![vec![vec![-1.0f64; n + 1]; n + 1]; n + 1];
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", calc(&mut dp, n, ijk[0], ijk[1], ijk[2]));
 }

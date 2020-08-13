c23cddd8d836339699d9b5110bd2586e02ff8a0d
hasevent <hasevent@gmail.com>
2020-08-13 16:19:38 +0900

    [update] m is clear.

--- src/bin/m.rs
+++ src/bin/m.rs
@@ -1,17 +1,34 @@
-//use proconio::{input, fastout};
 use proconio::input;
 
-//#[fastout]
+const M: u64 = 1000_000_007;
+
+fn calc(n: usize, k: usize, a: &Vec<usize>) -> u64 {
+    let mut dp = vec![vec![0; k + 1]; n];
+    for j in 0..=a[0] {
+        dp[0][j] = 1;
+    }
+    for j in (a[0] + 1)..=k {
+        dp[0][j] = 0;
+    }
+    for i in 1..n {
+        let mut cum = vec![0; k + 2];
+        for j in 1..=(k + 1) {
+            cum[j] = (cum[j - 1] + dp[i - 1][j - 1]) % M;
+        }
+        for j in 0..=k {
+            let ks = if a[i] > j { 0 } else {j - a[i]};
+            dp[i][j] = (cum[j + 1] + M - cum[ks]) % M;
+        }
+    }
+    dp[n - 1][k]
+}
+
 fn main() {
     input! {
-        n: i32,
-        d: [(i32, i32); n],
-    }
-    let r = n;
-    for (a, b) in d {
-        todo!();
+        n: usize,
+        k: usize,
+        a: [usize; n],
     }
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", calc(n, k, &a));
 }

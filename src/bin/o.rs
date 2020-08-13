a48a8343a268dbbc3cc803c5d7e7b33161e01ecc
hasevent <hasevent@gmail.com>
2020-08-14 01:07:14 +0900

    [update] o is clear.

--- src/bin/o.rs
+++ src/bin/o.rs
@@ -1,28 +1,39 @@
+use proconio::input;
+
 const M: u64 = 1000_000_007;
 
 fn main() {
-    proconio::input! {
-        n: usize,
-        a: [[u8; n]; n],
+    input! { n: usize }
+
+    let mut a = vec![0; n];
+    for i in 0..n {
+        for j in 0..n {
+            input! { aij: usize };
+            a[i] |= aij << j;
+        }
     }
 
-    println!("{}", calc(0, n, a));
+    let mut dp = vec![vec![None; 1 << n]; n + 1];
+    println!("{}", calc(0, n, 0, &a, &mut dp));
 }
 
-fn calc(i: usize, n: usize, a: Vec<Vec<u8>>) -> u64 {
-    if i == n {
-        1
+fn calc(i: usize, n: usize, p: usize, a: &Vec<usize>, dp: &mut Vec<Vec<Option<u64>>>) -> u64 {
+    if let Some(v) = dp[i][p] {
+        v
     } else {
-        let mut v = 0;
-        for j in 0..n {
-            if a[i][j] == 1 {
-                let mut a = a.clone();
-                for k in 0..n {
-                    a[k][j] = 0;
+        let v = if i == n {
+            1
+        } else {
+            let rest = a[i] & !p;
+            let mut v = 0;
+            for j in 0..n {
+                if (rest >> j) & 1 == 1 {
+                    v = (v + calc(i + 1, n, p | (1 << j), a, dp)) % M;
                 }
-                v += calc(i + 1, n, a) % M;
             }
-        }
+            v
+        };
+        dp[i][p] = Some(v);
         v
     }
 }

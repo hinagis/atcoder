e8788c8e61716134b13f9ee2aa6f602397aaa39c
hasevent <hasevent@gmail.com>
2020-08-13 19:33:12 +0900

    [update] n is miss.

--- src/bin/n.rs
+++ src/bin/n.rs
@@ -4,11 +4,26 @@ fn main() {
         a: [u64; n],
     }
 
-    let mut r = 0;
-    for i in 0..n {
-        r += a[i];
-    }
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", calc(a));
+}
+
+fn calc(a: Vec<u64>) -> u64 {
+    let n = a.len();
+    if n == 2 {
+        a[0] + a[1]
+    } else {
+        let mut v = 0;
+        for i in 0..(n - 1) {
+            let mut a = a.clone();
+            a[i] += a[i + 1];
+            a.remove(i + 1);
+            v = if v == 0 {
+                a[i] + calc(a)
+            } else {
+                std::cmp::min(v, a[i] + calc(a))
+            }
+        }
+        v
+    }
 }

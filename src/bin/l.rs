e67bd032e7252d66a2f08efede20297ddf571aae
hasevent <hasevent@gmail.com>
2020-08-13 10:37:11 +0900

    [update] l is clear.

--- src/bin/l.rs
+++ src/bin/l.rs
@@ -1,17 +1,41 @@
-//use proconio::{input, fastout};
 use proconio::input;
 
-//#[fastout]
-fn main() {
-    input! {
-        n: i32,
-        d: [(i32, i32); n],
-    }
-    let r = n;
-    for (a, b) in d {
-        todo!();
+struct DP {
+    dp: Vec<Vec<Option<i64>>>,
+    a: Vec<i64>,
+}
+
+impl DP {
+    fn new(n: usize, a: Vec<i64>) -> DP {
+        DP {
+            dp: vec![vec![None; n]; n],
+            a: a,
+        }
     }
 
-    println!("Yes");
-    println!("{}", r);
+    fn calc(self: &mut DP, l: usize, r: usize) -> i64 {
+        if let Some(v) = self.dp[l][r] {
+            v
+        } else {
+            let v = if l == r {
+                self.a[l]
+            } else {
+                std::cmp::max(
+                    self.a[l] - self.calc(l + 1, r),
+                    self.a[r] - self.calc(l, r - 1)
+                )
+            };
+            self.dp[l][r] = Some(v);
+            v
+        }
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    println!("{}", DP::new(n, a).calc(0, n - 1));
 }

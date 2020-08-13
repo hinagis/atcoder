5c9974eb20bb8890f08780df628ae8dc6592156c
hasevent <hasevent@gmail.com>
2020-08-14 00:29:25 +0900

    [update] o is miss.

--- src/bin/o.rs
+++ src/bin/o.rs
@@ -1,14 +1,28 @@
+const M: u64 = 1000_000_007;
+
 fn main() {
     proconio::input! {
         n: usize,
-        a: [u64; n],
+        a: [[u8; n]; n],
     }
 
-    let mut r = 0;
-    for i in 0..n {
-        r += a[i];
-    }
+    println!("{}", calc(0, n, a));
+}
 
-    println!("Yes");
-    println!("{}", r);
+fn calc(i: usize, n: usize, a: Vec<Vec<u8>>) -> u64 {
+    if i == n {
+        1
+    } else {
+        let mut v = 0;
+        for j in 0..n {
+            if a[i][j] == 1 {
+                let mut a = a.clone();
+                for k in 0..n {
+                    a[k][j] = 0;
+                }
+                v += calc(i + 1, n, a) % M;
+            }
+        }
+        v
+    }
 }

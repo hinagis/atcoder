348900b6e7cb5bc68ef325d4b47d1b7c3af53c30
hasevent <hasevent@gmail.com>
2020-08-15 18:32:50 +0900

    [update] q refactored, but still miss.

--- src/bin/q.rs
+++ src/bin/q.rs
@@ -1,36 +1,30 @@
 use proconio::{input, marker::Usize1};
-use num::bigint::BigUint;
 
 fn main() {
     input! {
         n: usize,
         h: [Usize1; n],
-        a: [u32; n],
+        a: [u64; n],
     }
 
-    let mut dp = vec![vec![BigUint::new(vec![0]); n]; n];
+    let mut dp = vec![0; n];
     for j in h[0]..n {
-        dp[0][j] += a[0];
+        dp[j] = a[0];
     }
     for i in 1..n {
-        for j in 0..h[i] {
-            dp[i][j] = dp[i - 1][j].clone();
-        }
-        dp[i][h[i]] = dp[i - 1][h[i]].clone() + a[i];
+        dp[h[i]] += a[i];
         for j in (h[i] + 1)..n {
-            dp[i][j] = if dp[i - 1][j] > dp[i][h[i]] {
-                dp[i - 1][j].clone()
-            } else {
-                dp[i][h[i]].clone()
+            if dp[j] < dp[h[i]] {
+                dp[j] = dp[h[i]]
             }
         }
     }
 
-    let mut m = &BigUint::new(vec![0]);
+    let mut m = 0;
     for j in 0..n {
-        if dp[n - 1][j] > *m {
-            m = &dp[n - 1][j]
+        if dp[j] > m {
+            m = dp[j]
         }
     }
-    println!("{}", *m);
+    println!("{}", m);
 }

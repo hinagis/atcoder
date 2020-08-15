b0788f680b3a5e30c199ff2f5b68a6e135777b31
hasevent <hasevent@gmail.com>
2020-08-15 13:55:09 +0900

    [update] p refactored without clone.

--- src/bin/p.rs
+++ src/bin/p.rs
@@ -8,36 +8,34 @@ fn main() {
         xy: [(Usize1, Usize1); n - 1],
     }
 
-    println!("{}", DP::new(n, &xy).calc(0, 0));
+    let mut t = vec![vec![]; n];
+    for (x, y) in xy {
+        t[x].push(y);
+        t[y].push(x);
+    }
+
+    println!("{}", DP::new(n).calc(&t, 0, 0));
 }
 
 struct DP {
     dp: Vec<Option<(u64,u64)>>,
-    t: Vec<Vec<usize>>,
 }
 
 impl DP {
-    fn new(n: usize, xy: &Vec<(usize, usize)>) -> DP {
-        let mut t = vec![vec![]; n];
-        for &(x, y) in xy {
-            t[x].push(y);
-            t[y].push(x);
-        }
-
+    fn new(n: usize) -> DP {
         DP {
             dp: vec![None; n],
-            t: t,
         }
     }
 
-    fn calc(&mut self, i: usize, p: usize) -> u64 {
+    fn calc(&mut self, t: &Vec<Vec<usize>>, i: usize, p: usize) -> u64 {
         let v = if let Some(v) = self.dp[i] {
             v
         } else {
             let mut v = (1, 1);
-            for j in self.t[i].clone() {
+            for &j in t[i].iter() {
                 if j != p {
-                    v.0 = v.0 * self.calc(j, i) % M;
+                    v.0 = v.0 * self.calc(t, j, i) % M;
                     v.1 = v.1 * self.dp[j].unwrap().0 % M;
                 }
             }

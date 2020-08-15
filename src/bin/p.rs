18f166033c68f4dede8e2b29eb018e403c4247c9
hasevent <hasevent@gmail.com>
2020-08-15 13:53:59 +0900

    [update] p refactored a little.

--- src/bin/p.rs
+++ src/bin/p.rs
@@ -1,41 +1,27 @@
-use std::collections::HashMap;
+use proconio::{input, marker::Usize1};
 
 const M: u64 = 1000_000_007;
 
 fn main() {
-    proconio::input! {
+    input! {
         n: usize,
-        xy: [(usize, usize); n - 1],
+        xy: [(Usize1, Usize1); n - 1],
     }
 
-    println!("{}", DP::new(n, &xy).calc(1));
+    println!("{}", DP::new(n, &xy).calc(0, 0));
 }
 
 struct DP {
     dp: Vec<Option<(u64,u64)>>,
-    t: HashMap<usize, Vec<usize>>,
+    t: Vec<Vec<usize>>,
 }
 
 impl DP {
     fn new(n: usize, xy: &Vec<(usize, usize)>) -> DP {
-        let mut g = HashMap::new();
-        for (x, y) in xy {
-            g.entry(*x).or_insert(Vec::new()).push(*y);
-            g.entry(*y).or_insert(Vec::new()).push(*x);
-        }
-
-        let mut t = HashMap::new();
-        if let Some(c) = g.get_mut(&1) {
-            let mut p = vec![(1, t.entry(1).or_insert(c.clone()).clone())];
-            while p.is_empty() == false {
-                for i in 0..p[0].1.len() {
-                    if let Some(c) = g.get_mut(&p[0].1[i]) {
-                        c.retain(|ci| *ci != p[0].0);
-                        p.push((p[0].1[i], t.entry(p[0].1[i]).or_insert(c.clone()).clone()));
-                    }
-                }
-                p.remove(0);
-            }
+        let mut t = vec![vec![]; n];
+        for &(x, y) in xy {
+            t[x].push(y);
+            t[y].push(x);
         }
 
         DP {
@@ -44,22 +30,21 @@ impl DP {
         }
     }
 
-    fn calc(&mut self, i: usize) -> u64 {
-        if let Some(v) = self.dp[i - 1] {
-            (v.0 + v.1) % M
+    fn calc(&mut self, i: usize, p: usize) -> u64 {
+        let v = if let Some(v) = self.dp[i] {
+            v
         } else {
             let mut v = (1, 1);
-            if self.t.contains_key(&i) {
-                let clen = { self.t.get(&i).unwrap().len() };
-                for j in 0..clen {
-                    let j = { self.t.get(&i).unwrap()[j] };
-                    v.0 = (v.0 * self.calc(j)) % M;
-                    v.1 = (v.1 * self.dp[j - 1].unwrap().0) % M;
+            for j in self.t[i].clone() {
+                if j != p {
+                    v.0 = v.0 * self.calc(j, i) % M;
+                    v.1 = v.1 * self.dp[j].unwrap().0 % M;
                 }
             }
-            self.dp[i - 1] = Some(v);
-            (v.0 + v.1) % M
-    }
+            self.dp[i] = Some(v);
+            v
+        };
+        (v.0 + v.1) % M
     }
 }
 

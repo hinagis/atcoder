d0c97077c4e7f8b655f15fa693e070a013c4e691
hasevent <hasevent@gmail.com>
2020-08-17 11:43:07 +0900

    [update] e refactored, into struct.

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -8,50 +8,73 @@ fn main() {
         rcv: [(Usize1, Usize1, u64); k],
     }
 
-    let b = make_board(&rcv, r, c);
+    println!("{}", DP::new(&rcv, r, c).calc());
+}
 
-    let mut dp = vec![vec![vec![None; 3 + 1]; c]; r];
-    dp[0][0][0] = Some(0);
-    for i in 0..r {
-        for j in 0..c {
-            for k in (0..3).rev() {
-                if let Some(v) = dp[i][j][k] {
-                    set_v(&mut dp, i, j, k + 1, v + b[i][j])
-                }
-            }
-            for k in 0..=3 {
-                if let Some(v) = dp[i][j][k] {
-                    if i + 1 < r {
-                        set_v(&mut dp, i + 1, j, 0, v)
+struct DP {
+    dp: Vec<Vec<Vec<Option<u64>>>>,
+    b: Vec<Vec<u64>>,
+    r: usize,
+    c: usize,
+}
+
+impl DP {
+    fn new(rcv: &Vec<(usize, usize, u64)>, r: usize, c: usize) -> DP {
+        DP {
+            dp: vec![vec![vec![None; 3 + 1]; c]; r],
+            b: DP::make_board(&rcv, r, c),
+            r: r,
+            c: c,
+        }
+    }
+
+    fn make_board(rcv: &Vec<(usize, usize, u64)>, r: usize, c: usize) -> Vec<Vec<u64>> {
+        let mut b = vec![vec![0; c]; r];
+        for &(i, j, v) in rcv {
+            b[i][j] = v;
+        }
+        b
+    }
+
+    fn calc(&mut self) -> u64 {
+        let (r, c) = (self.r, self.c);
+        self.dp[0][0][0] = Some(0);
+        for i in 0..r {
+            for j in 0..c {
+                for k in (0..3).rev() {
+                    if let Some(v) = self.dp[i][j][k] {
+                        self.set_v(i, j, k + 1, v + self.b[i][j]);
                     }
-                    if j + 1 < c {
-                        set_v(&mut dp, i, j + 1, k, v)
+                }
+                for k in 0..=3 {
+                    if let Some(v) = self.dp[i][j][k] {
+                        if i + 1 < r {
+                            self.set_v(i + 1, j, 0, v);
+                        }
+                        if j + 1 < c {
+                            self.set_v(i, j + 1, k, v);
+                        }
                     }
                 }
             }
         }
+    
+        self.get_ans()
     }
 
-    let mut ans = 0;
-    for k in 0..=3 {
-        if let Some(v) = dp[r - 1][c - 1][k] {
-            ans = ans.max(v);
+    fn set_v(&mut self, i: usize, j: usize, k: usize, v: u64) {
+        let nv = &mut self.dp[i][j][k];
+        let v = if let Some(nv) = *nv { nv.max(v) } else { v };
+        *nv = Some(v);
+    }
+
+    fn get_ans(&self) -> u64 {
+        let mut ans = 0;
+        for k in 0..=3 {
+            if let Some(v) = self.dp[self.r - 1][self.c - 1][k] {
+                ans = ans.max(v);
+            }
         }
+        ans
     }
-
-    println!("{}", ans);
-}
-
-fn set_v(dp: &mut Vec<Vec<Vec<Option<u64>>>>, i: usize, j: usize, k: usize, v: u64) {
-    let nv = &mut dp[i][j][k];
-    let v = if let Some(nv) = *nv { nv.max(v) } else { v };
-    *nv = Some(v);
-}
-
-fn make_board(rcv: &Vec<(usize, usize, u64)>, r: usize, c: usize) -> Vec<Vec<u64>> {
-    let mut b = vec![vec![0; c]; r];
-    for &(i, j, v) in rcv {
-        b[i][j] = v;
-    }
-    b
 }

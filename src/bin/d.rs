cfae97655aa433ee3f3cb201ee7e80520c24a316
hasevent <hasevent@gmail.com>
2020-08-23 00:44:19 +0900

    [update] d cleared.

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -11,57 +11,12 @@ fn main() {
     let mut dp = vec![vec![u32::max_value(); w]; h];
     dp[c.0][c.1] = 0;
     let mut q = vec![(c.0, c.1)];
-    let mut r = u32::max_value();
-    let mut wq = vec![];
-    while let Some(c) = q.pop() {
-        if c.0 == d.0 && c.1 == d.1 {
-            r = r.min(dp[c.0][c.1]);
-        } else {
-            let v = dp[c.0][c.1];
-            if c.0 > 0     && v < dp[c.0 - 1][c.1] && s[c.0 - 1][c.1] == '.' { dp[c.0 - 1][c.1] = v; q.push((c.0 - 1, c.1)); }
-            if c.0 + 1 < h && v < dp[c.0 + 1][c.1] && s[c.0 + 1][c.1] == '.' { dp[c.0 + 1][c.1] = v; q.push((c.0 + 1, c.1)); }
-            if c.1 > 0     && v < dp[c.0][c.1 - 1] && s[c.0][c.1 - 1] == '.' { dp[c.0][c.1 - 1] = v; q.push((c.0, c.1 - 1)); }
-            if c.1 + 1 < w && v < dp[c.0][c.1 + 1] && s[c.0][c.1 + 1] == '.' { dp[c.0][c.1 + 1] = v; q.push((c.0, c.1 + 1)); }
-
-            if c.0 > 1                    && v + 1 < dp[c.0 - 2][c.1    ] && s[c.0 - 2][c.1    ] == '.' { dp[c.0 - 2][c.1    ] = v + 1; wq.push((c.0 - 2, c.1    )); }
-            if c.0 > 0     && c.1 > 0     && v + 1 < dp[c.0 - 1][c.1 - 1] && s[c.0 - 1][c.1 - 1] == '.' { dp[c.0 - 1][c.1 - 1] = v + 1; wq.push((c.0 - 1, c.1 - 1)); }
-            if                c.1 > 1     && v + 1 < dp[c.0    ][c.1 - 2] && s[c.0    ][c.1 - 2] == '.' { dp[c.0    ][c.1 - 2] = v + 1; wq.push((c.0    , c.1 - 2)); }
-            if c.0 + 1 < h && c.1 > 0     && v + 1 < dp[c.0 + 1][c.1 - 1] && s[c.0 + 1][c.1 - 1] == '.' { dp[c.0 + 1][c.1 - 1] = v + 1; wq.push((c.0 + 1, c.1 - 1)); }
-            if c.0 + 2 < h                && v + 1 < dp[c.0 + 2][c.1    ] && s[c.0 + 2][c.1    ] == '.' { dp[c.0 + 2][c.1    ] = v + 1; wq.push((c.0 + 2, c.1    )); }
-            if c.0 + 1 < h && c.1 + 1 < w && v + 1 < dp[c.0 + 1][c.1 + 1] && s[c.0 + 1][c.1 + 1] == '.' { dp[c.0 + 1][c.1 + 1] = v + 1; wq.push((c.0 + 1, c.1 + 1)); }
-            if                c.1 + 2 < w && v + 1 < dp[c.0    ][c.1 + 2] && s[c.0    ][c.1 + 2] == '.' { dp[c.0    ][c.1 + 2] = v + 1; wq.push((c.0    , c.1 + 2)); }
-            if c.0 > 0     && c.1 + 1 < w && v + 1 < dp[c.0 - 1][c.1 + 1] && s[c.0 - 1][c.1 + 1] == '.' { dp[c.0 - 1][c.1 + 1] = v + 1; wq.push((c.0 - 1, c.1 + 1)); }
-
-            if c.0 > 1     && c.1 > 1     && v + 1 < dp[c.0 - 2][c.1 - 2] && s[c.0 - 2][c.1 - 2] == '.' { dp[c.0 - 2][c.1 - 2] = v + 1; wq.push((c.0 - 2, c.1 - 2)); }
-            if c.0 + 2 < h && c.1 > 1     && v + 1 < dp[c.0 + 2][c.1 - 2] && s[c.0 + 2][c.1 - 2] == '.' { dp[c.0 + 2][c.1 - 2] = v + 1; wq.push((c.0 + 2, c.1 - 2)); }
-            if c.0 + 2 < h && c.1 + 2 < w && v + 1 < dp[c.0 + 2][c.1 + 2] && s[c.0 + 2][c.1 + 2] == '.' { dp[c.0 + 2][c.1 + 2] = v + 1; wq.push((c.0 + 2, c.1 + 2)); }
-            if c.0 > 1     && c.1 + 2 < w && v + 1 < dp[c.0 - 2][c.1 + 2] && s[c.0 - 2][c.1 + 2] == '.' { dp[c.0 - 2][c.1 + 2] = v + 1; wq.push((c.0 - 2, c.1 + 2)); }
-        }
-        while q.is_empty() && !wq.is_empty() {
-            let c = wq.pop().unwrap();
-            if c.0 == d.0 && c.1 == d.1 {
-                r = r.min(dp[c.0][c.1]);
-            } else {
-                let v = dp[c.0][c.1];
-                if c.0 > 0     && v < dp[c.0 - 1][c.1] && s[c.0 - 1][c.1] == '.' { dp[c.0 - 1][c.1] = v; q.push((c.0 - 1, c.1)); }
-                if c.0 + 1 < h && v < dp[c.0 + 1][c.1] && s[c.0 + 1][c.1] == '.' { dp[c.0 + 1][c.1] = v; q.push((c.0 + 1, c.1)); }
-                if c.1 > 0     && v < dp[c.0][c.1 - 1] && s[c.0][c.1 - 1] == '.' { dp[c.0][c.1 - 1] = v; q.push((c.0, c.1 - 1)); }
-                if c.1 + 1 < w && v < dp[c.0][c.1 + 1] && s[c.0][c.1 + 1] == '.' { dp[c.0][c.1 + 1] = v; q.push((c.0, c.1 + 1)); }
-    
-                if c.0 > 1                    && v + 1 < dp[c.0 - 2][c.1    ] && s[c.0 - 2][c.1    ] == '.' { dp[c.0 - 2][c.1    ] = v + 1; wq.push((c.0 - 2, c.1    )); }
-                if c.0 > 0     && c.1 > 0     && v + 1 < dp[c.0 - 1][c.1 - 1] && s[c.0 - 1][c.1 - 1] == '.' { dp[c.0 - 1][c.1 - 1] = v + 1; wq.push((c.0 - 1, c.1 - 1)); }
-                if                c.1 > 1     && v + 1 < dp[c.0    ][c.1 - 2] && s[c.0    ][c.1 - 2] == '.' { dp[c.0    ][c.1 - 2] = v + 1; wq.push((c.0    , c.1 - 2)); }
-                if c.0 + 1 < h && c.1 > 0     && v + 1 < dp[c.0 + 1][c.1 - 1] && s[c.0 + 1][c.1 - 1] == '.' { dp[c.0 + 1][c.1 - 1] = v + 1; wq.push((c.0 + 1, c.1 - 1)); }
-                if c.0 + 2 < h                && v + 1 < dp[c.0 + 2][c.1    ] && s[c.0 + 2][c.1    ] == '.' { dp[c.0 + 2][c.1    ] = v + 1; wq.push((c.0 + 2, c.1    )); }
-                if c.0 + 1 < h && c.1 + 1 < w && v + 1 < dp[c.0 + 1][c.1 + 1] && s[c.0 + 1][c.1 + 1] == '.' { dp[c.0 + 1][c.1 + 1] = v + 1; wq.push((c.0 + 1, c.1 + 1)); }
-                if                c.1 + 2 < w && v + 1 < dp[c.0    ][c.1 + 2] && s[c.0    ][c.1 + 2] == '.' { dp[c.0    ][c.1 + 2] = v + 1; wq.push((c.0    , c.1 + 2)); }
-                if c.0 > 0     && c.1 + 1 < w && v + 1 < dp[c.0 - 1][c.1 + 1] && s[c.0 - 1][c.1 + 1] == '.' { dp[c.0 - 1][c.1 + 1] = v + 1; wq.push((c.0 - 1, c.1 + 1)); }
-
-                if c.0 > 1     && c.1 > 1     && v + 1 < dp[c.0 - 2][c.1 - 2] && s[c.0 - 2][c.1 - 2] == '.' { dp[c.0 - 2][c.1 - 2] = v + 1; wq.push((c.0 - 2, c.1 - 2)); }
-                if c.0 + 2 < h && c.1 > 1     && v + 1 < dp[c.0 + 2][c.1 - 2] && s[c.0 + 2][c.1 - 2] == '.' { dp[c.0 + 2][c.1 - 2] = v + 1; wq.push((c.0 + 2, c.1 - 2)); }
-                if c.0 + 2 < h && c.1 + 2 < w && v + 1 < dp[c.0 + 2][c.1 + 2] && s[c.0 + 2][c.1 + 2] == '.' { dp[c.0 + 2][c.1 + 2] = v + 1; wq.push((c.0 + 2, c.1 + 2)); }
-                if c.0 > 1     && c.1 + 2 < w && v + 1 < dp[c.0 - 2][c.1 + 2] && s[c.0 - 2][c.1 + 2] == '.' { dp[c.0 - 2][c.1 + 2] = v + 1; wq.push((c.0 - 2, c.1 + 2)); }
-            }
+    let mut v = 0;
+    while !q.is_empty() {
+        q = move_a(&mut dp, &mut q, v, &s, h, w);
+        if !q.is_empty() {
+            v += 1;
+            q = move_b(&mut dp, &q, v, &s, h, w);
         }
     }
 
@@ -70,4 +25,61 @@ fn main() {
     } else {
         println!("-1");
     }
+}
+
+fn move_a(
+    dp: &mut Vec<Vec<u32>>,
+    q: &mut Vec<(usize, usize)>,
+    v: u32,
+    s: &Vec<Vec<char>>,
+    h: usize,
+    w: usize,
+) -> Vec<(usize, usize)> {
+    let mut nq = vec![];
+    while let Some(c) = q.pop() {
+        nq.push(c);
+        if c.0 > 0     { let c = (c.0 - 1, c.1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; q.push(c); } }
+        if c.0 + 1 < h { let c = (c.0 + 1, c.1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; q.push(c); } }
+        if c.1 > 0     { let c = (c.0, c.1 - 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; q.push(c); } }
+        if c.1 + 1 < w { let c = (c.0, c.1 + 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; q.push(c); } }
+    }
+    nq
+}
+
+fn move_b(
+    dp: &mut Vec<Vec<u32>>,
+    q: &Vec<(usize, usize)>,
+    v: u32,
+    s: &Vec<Vec<char>>,
+    h: usize,
+    w: usize,
+) -> Vec<(usize, usize)> {
+    let mut nq = vec![];
+    for c in q {
+        if c.0 > 1     && c.1 > 1     { let c = (c.0 - 2, c.1 - 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 1     && c.1 > 0     { let c = (c.0 - 2, c.1 - 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 1                    { let c = (c.0 - 2, c.1    ); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 1     && c.1 + 1 < w { let c = (c.0 - 2, c.1 + 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 1     && c.1 + 2 < w { let c = (c.0 - 2, c.1 + 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+
+        if c.0 > 0     && c.1 > 1     { let c = (c.0 - 1, c.1 - 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 0     && c.1 > 0     { let c = (c.0 - 1, c.1 - 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 0     && c.1 + 1 < w { let c = (c.0 - 1, c.1 + 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 > 0     && c.1 + 2 < w { let c = (c.0 - 1, c.1 + 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+
+        if                c.1 > 1     { let c = (c.0    , c.1 - 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if                c.1 + 2 < w { let c = (c.0    , c.1 + 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+
+        if c.0 + 1 < h && c.1 > 1     { let c = (c.0 + 1, c.1 - 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 1 < h && c.1 > 0     { let c = (c.0 + 1, c.1 - 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 1 < h && c.1 + 1 < w { let c = (c.0 + 1, c.1 + 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 1 < h && c.1 + 2 < w { let c = (c.0 + 1, c.1 + 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+
+        if c.0 + 2 < h && c.1 > 1     { let c = (c.0 + 2, c.1 - 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 2 < h && c.1 > 0     { let c = (c.0 + 2, c.1 - 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 2 < h                { let c = (c.0 + 2, c.1    ); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 2 < h && c.1 + 1 < w { let c = (c.0 + 2, c.1 + 1); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+        if c.0 + 2 < h && c.1 + 2 < w { let c = (c.0 + 2, c.1 + 2); if v < dp[c.0][c.1] && s[c.0][c.1] == '.' { dp[c.0][c.1] = v; nq.push(c); } }
+    }
+    nq
 }

fab56333a3d02f6a4e27bb58fab2232892723ea5
hasevent <hasevent@gmail.com>
2020-08-23 12:13:16 +0900

    [update] e cleared.

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -6,21 +6,38 @@ fn main() {
         m: usize,
         hw: [(Usize1, Usize1); m]
     }
-    let mut sum_h = vec![0; h];
+    let mut sum_h = vec![std::collections::HashSet::new(); h];
     let mut sum_v = vec![0; w];
-    let mut bomb = vec![vec![false; w]; h];
+    let mut max_h = 0;
+    let mut max_v = 0;
     for (i, j) in hw {
-        bomb[i][j] = true;
-        sum_h[i] += 1;
+        sum_h[i].insert(j);
         sum_v[j] += 1;
+        max_h = max_h.max(sum_h[i].len());
+        max_v = max_v.max(sum_v[j]);
     }
 
-    let mut r = 0;
-    for i in 0..h {
-        for j in 0..w {
-            r = r.max(sum_h[i] + sum_v[j] - if bomb[i][j] { 1 } else { 0 });
+    let mut max_vs = std::collections::HashSet::new();
+    for j in 0..w {
+        if sum_v[j] >= max_v {
+            max_vs.insert(j);
         }
     }
 
-    println!("{}", r);
+    let mut f = false;
+    for i in 0..h {
+        if sum_h[i].len() >= max_h {
+            for j in &max_vs {
+                if !sum_h[i].contains(&j) {
+                    f = true;
+                    break;
+                }
+            }
+            if f {
+                break;
+            }
+        }
+    }
+
+    println!("{}", max_h + max_v - if f { 0 } else { 1 });
 }

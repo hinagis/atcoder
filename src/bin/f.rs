6f1a253d0d67328dda4fb75630423eaee408470e
hasevent <hasevent@gmail.com>
2020-08-29 17:35:49 +0900

    [update] f refactored.

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,6 +1,13 @@
 use proconio::{input, marker::Usize1};
 
-fn chmax(a: &mut i32, b: i32) { *a = (*a).max(b); }
+fn chmax(a: &mut Option<u32>, b: u32) {
+    *a = Some(if let Some(v) = *a {
+        v.max(b)
+    } else {
+        b
+    });
+}
+
 fn rotate(x: &mut usize, y: &mut usize, z: &mut usize) {
     let w = *x;
     *x = *y;
@@ -8,34 +15,37 @@ fn rotate(x: &mut usize, y: &mut usize, z: &mut usize) {
     *z = w;
 }
 
+fn get_v(m: u32, l: Option<u32>) -> u32 {
+    if let Some(l) = l {
+        m.max(l + 1)
+    } else {
+        m
+    }
+}
+
 struct DP {
-    l12: Vec<Vec<i32>>,
-    l1_max: Vec<i32>,
-    l2_max: Vec<i32>,
-    max: i32,
+    l12: Vec<Vec<Option<u32>>>,
+    max: (u32, Vec<Option<u32>>),
     n: usize,
 }
 
 impl DP {
     fn new(n: usize) -> DP {
         DP {
-            l12: vec![vec![i32::min_value(); n + 1]; n + 1],
-            l1_max: vec![i32::min_value(); n],
-            l2_max: vec![i32::min_value(); n],
-            max: i32::min_value(),
+            l12: vec![vec![None; n]; n],
+            max: (0, vec![None; n]),
             n: n,
         }
     }
-    fn set(&mut self, l1: usize, l2: usize, v: i32) {
+
+    fn set(&mut self, l1: usize, l2: usize, v: u32) {
         if l2 != self.n {
             chmax(&mut self.l12[l1][l2], v);
             chmax(&mut self.l12[l2][l1], v);
-            chmax(&mut self.l1_max[l2], v);
-            chmax(&mut self.l2_max[l2], v);
+            chmax(&mut self.max.1[l2], v);
         }
-        chmax(&mut self.l1_max[l1], v);
-        chmax(&mut self.l2_max[l1], v);
-        chmax(&mut self.max, v);
+        chmax(&mut self.max.1[l1], v);
+        self.max.0 = self.max.0.max(v);
     }
 }
 
@@ -47,37 +57,37 @@ fn main() {
 
     let mut dp = DP::new(n);
     dp.set(a[0], a[1], 0);
-    let mut mc = 0;
+    let mut c = 0; // counter, number of cuts 3 to 5 from the left.
     for i in 1..n {
         let (mut x, mut y, mut z) = (a[i * 3 - 1], a[i * 3], a[i * 3 + 1]);
         if x == y && x == z {
-            mc += 1;
+            c += 1;
         } else {
+            let m = dp.max.0;
             let mut q = vec![];
             for _ in 0..3 {
                 for l in 0..n {
-                    let mut v = dp.l1_max[l];
-                    if y == z {
-                        v = v.max(dp.l12[y][l] + 1);
+                    if let Some(m) = dp.max.1[l] {
+                        let v = if y == z { get_v(m, dp.l12[l][y]) } else { m };
+                        q.push((l, x, v));
                     }
-                    q.push((x, l, v));
                 }
-                let mut v = dp.max;
-                if y == z {
-                    v = v.max(dp.l1_max[y] + 1);
-                }
+                let v = if y == z { get_v(m, dp.max.1[y]) } else { m };
                 q.push((x, n, v));
 
-                let v = dp.max.max(dp.l12[z][z] + 1);
+                let v = get_v(m, dp.l12[z][z]);
                 q.push((x, y, v));
+
                 rotate(&mut x, &mut y, &mut z);
             }
+
             for (x, y, v) in q {
                 dp.set(x, y, v);
             }
         }
     }
+
     let l = a[3 * n - 1];
-    let v = dp.max.max(dp.l12[l][l] + 1);
-    println!("{}", v + mc);
+    let v = get_v(dp.max.0, dp.l12[l][l]);
+    println!("{}", v + c);
 }

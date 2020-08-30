5979298b426fdfe175499449f10d756ddd3f59a4
hasevent <hasevent@gmail.com>
2020-08-30 16:46:54 +0900

    [update] f is TLE.

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,9 +1,38 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input, marker::Usize1};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        ab: [(Usize1, Usize1); h],
+    }
+
+    let mut dp = vec![i32::max_value(); w];
+    for j in 0..w {
+        dp[j] = 0;
+    }
+    for i in 1..=h {
+        let mut v = if dp[0] < i32::max_value() && ab[i - 1].0 > 0  {
+            dp[0] + 1
+        } else {
+            i32::max_value()
+        };
+        dp[0] = v;
+        let mut min = v;
+        for j in 1..w {
+            if v < i32::max_value() {
+                v += 1;
+            }
+            if dp[j] < i32::max_value() && j < ab[i - 1].0 || j > ab[i - 1].1 {
+                v = v.min(dp[j] + 1);
+            }
+            dp[j] = v;
+            min = min.min(v);
+        }
+        println!("{}", if min == i32::max_value() {
+            -1
+        } else {
+            min
+        });
+    }
 }

0a4d234157edb79211cd6dbb4f0416f73cc6bc8f
hasevent <hasevent@gmail.com>
2020-09-11 18:25:46 +0900

    [update] c cleared.

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,9 +1,32 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::input;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! { t: usize }
+
+    let mut ans = vec![];
+    for _ in 0..t {
+        input! { n: u64, m: u64, a: u64, b: u64 }
+        ans.push(floor_sum(n, m, a, b).to_string());
+    }
+    println!("{}", ans.join("\n"));
+}
+
+fn floor_sum(n: u64, m: u64, mut a: u64, mut b: u64) -> u64 {
+    let mut r = 0;
+    if a >= m {
+        r += (n - 1) * n * (a / m) / 2;
+        a %= m;
+    }
+    if b >= m {
+        r += n * (b / m);
+        b %= m;
+    }
+
+    let y = (a * n + b) / m;
+    if y > 0 {
+        let x = y * m - b;
+        r += (n - (x + a - 1) / a) * y;
+        r += floor_sum(y, a, m, (a - x % a) % a);
+    }
+    r
 }

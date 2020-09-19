19e0b6d4d52c73ca82ea95b2630269d0188cf950
hinagis <hasevent@gmail.com>
2020-09-19 17:29:16 +0900

    [update] e refactored.
    e changed to generics.

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -6,9 +6,10 @@ fn main() {
     let mut g = MinCostFlow::new(n * 2 + 2);
     let s = n * 2;
     let t = s + 1;
+    let kn = k * n as i64;
     const M: i64 = 1000000000;
 
-    g.add_edge(s, t, i64::max_value(), M);
+    g.add_edge(s, t, kn, M);
     for i in 0..n {
         g.add_edge(s, i, k, 0);
         g.add_edge(i + n, t, k, 0);
@@ -18,7 +19,6 @@ fn main() {
         }
     }
 
-    let kn = k * n as i64;
     let v = kn * M - g.flow(s, t, kn);
 
     let mut f = vec![vec!['.'; n]; n];
@@ -39,51 +39,52 @@ fn main() {
     );
 }
 
-use std::collections::VecDeque;
+use num::{Integer, Bounded, Signed, zero};
+use std::ops::{AddAssign, SubAssign};
 
 #[derive(Copy, Clone, Debug)]
-struct Edge {
+struct Edge<T: Clone> {
     to: usize,
     rev: usize,
-    v: i64,
-    cost: i64,
+    v: T,
+    cost: T,
 }
 
-struct MinCostFlow {
-    edges: Vec<Vec<Edge>>,
+struct MinCostFlow<T: Clone> {
+    edges: Vec<Vec<Edge<T>>>,
 }
 
-impl MinCostFlow {
+impl<T: Copy + Clone + Integer + Bounded + Signed + AddAssign + SubAssign> MinCostFlow<T> {
     fn new(n: usize) -> Self {
         Self {
             edges: vec![vec![]; n],
         }
     }
 
-    fn add_edge(&mut self, s: usize, t: usize, v: i64, cost: i64) {
+    fn add_edge(&mut self, s: usize, t: usize, v: T, cost: T) {
         let sid = self.edges[s].len();
         let tid = self.edges[t].len();
         self.edges[s].push(Edge { to: t, rev: tid, v, cost });
-        self.edges[t].push(Edge { to: s, rev: sid, v: 0, cost: -cost });
+        self.edges[t].push(Edge { to: s, rev: sid, v: zero(), cost: -cost });
     }
 
-    fn flow(&mut self, s: usize, t: usize, mut flow: i64) -> i64 {
+    fn flow(&mut self, s: usize, t: usize, mut flow: T) -> T {
         let n = self.edges.len();
-        let mut ans = 0;
+        let mut ans = zero();
         let mut from = vec![(0, 0); n];
 
-        while flow > 0 {
-            let mut dist = vec![i64::max_value(); n];
+        while flow > zero() {
+            let mut dist = vec![T::max_value(); n];
             let mut done = vec![false; n];
 
-            dist[s] = 0;
+            dist[s] = zero();
 
-            let mut q = VecDeque::new();
+            let mut q = std::collections::VecDeque::new();
             q.push_back(s);
             while let Some(s) = q.pop_front() {
                 done[s] = false;
                 for (i, &e) in self.edges[s].iter().enumerate() {
-                    if e.v > 0 {
+                    if e.v > zero() {
                         let cost = dist[s] + e.cost;
                         if cost < dist[e.to] {
                             dist[e.to] = cost;
@@ -97,7 +98,7 @@ impl MinCostFlow {
                 }
             }
 
-            if dist[t] == i64::max_value() {
+            if dist[t] == T::max_value() {
                 return dist[t];
             }
 
@@ -114,7 +115,7 @@ impl MinCostFlow {
             let mut to = t;
             while to != s {
                 let (f, i) = from[to];
-                let mut edge = &mut self.edges[f][i];
+                let edge = &mut self.edges[f][i];
                 edge.v -= v;
                 let rev = edge.rev;
                 self.edges[to][rev].v += v;

56bb332a10e799694fe2d08e098dd3cb184f0fc0
hinagis <hasevent@gmail.com>
2020-09-22 23:02:58 +0900

    [update] f cleared.

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,9 +1,29 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::input;
 
-    println!("Yes");
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    let mut wh = vec![(n, n - 2)];
+    let mut hw = vec![(n, n - 2)];
+    let mut r = (n - 2) * (n - 2);
+    for _ in 0..q {
+        input! {f: usize, x: usize}
+        let (wh, hw) = if f == 1 {(&mut wh, &mut hw)} else {(&mut hw, &mut wh)};
+        if x < wh.last().unwrap().0 {
+            let h = hw.last().unwrap().0;
+            r -= h - 2;
+            wh.push((x, h - 2));
+        } else {
+            for i in 0..wh.len() {
+                if x > wh[i].0 {
+                    r -= wh[i].1;
+                    break;
+                }
+            }
+        }
+    }
     println!("{}", r);
 }

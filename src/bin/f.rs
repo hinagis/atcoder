eb7ecc40407e790372efff87c147cd712a020abe
hinagis <hasevent@gmail.com>
2020-09-22 23:49:20 +0900

    [update] f refactored.
    still use binary-search.
    changed to closure.
    prevent to push same data.

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -12,27 +12,32 @@ fn main() {
     for _ in 0..q {
         input! {f: usize, x: usize}
         let (wh, hw) = if f == 1 {(&mut wh, &mut hw)} else {(&mut hw, &mut wh)};
-        if x < wh.last().unwrap().0 {
-            let h = hw.last().unwrap().0;
-            r -= h - 2;
-            wh.push((x, h - 2));
-        } else {
-            r -= wh[binsearch(x, wh)].1;
+        if let Some(w) = wh.last_mut() {
+            if x < w.0 {
+                let h = hw.last().unwrap().0 - 2;
+                r -= h;
+                if w.1 == h {
+                    (*w).0 = x;
+                } else {
+                    wh.push((x, h));
+                }
+            } else {
+                let bs = || {
+                    let mut l = 0;
+                    let mut r = wh.len();
+                    while l < r {
+                        let m = (l + r) / 2;
+                        if x < wh[m].0 {
+                            l = m + 1;
+                        } else {
+                            r = m;
+                        }
+                    }
+                    l
+                };
+                r -= wh[bs()].1;
+            }
         }
     }
     println!("{}", r);
-}
-
-fn binsearch(x: usize, wh: &Vec<(usize, usize)>) -> usize {
-    let mut l = 0;
-    let mut r = wh.len();
-    while l < r {
-        let m = (l + r) / 2;
-        if x < wh[m].0 {
-            l = m + 1;
-        } else {
-            r = m;
-        }
-    }
-    l
 }

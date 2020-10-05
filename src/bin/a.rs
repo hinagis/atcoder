7d03442ae1d338cce16a3d8b3f9764d6160d2028
Hinagi <hasevent@gmail.com>
2020-10-05 17:32:06 +0900

    a is cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,9 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        a: i32,
+        b: i32,
+    }
+    let x = (a + b) / 2;
+    let y = (a - b) / 2;
+    println!("{} {}", x, y);
 }

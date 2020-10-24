9e0cbfb10d44a83017663b8f2c5e79ca3800fb49
hinagis <hasevent@gmail.com>
2020-10-24 09:18:45 +0900

    a is cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,18 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::input;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! {
+        n: usize,
+        mut x: i32,
+        mut y: i32,
+    }
+
+    let mut s = 0;
+    for _ in 1..n {
+        input! {j: i32, i: i32}
+        s += (x - j).abs() + (y - i).abs();
+        x = j;
+        y = i;
+    }
+    println!("{}", s);
 }

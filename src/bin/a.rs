72204f1b29bd396ebae9069ab55cac54979e1bf7
hinagis <hasevent@gmail.com>
2020-11-22 23:03:44 +0900

    [at end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,9 @@
 fn main() {
     proconio::input! {
-        n: usize,
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
     }
-    let r = n;
-
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", a * d - b * c);
 }
--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,9 +1,20 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input, marker::Chars};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! {
+        _n: usize,
+        mut x: u32,
+        s: Chars,
+    }
+
+    for c in s {
+        if c == 'o' {
+            x += 1;
+        } else {
+            if x > 0 {
+                x -= 1;
+            }
+        }
+    }
+    println!("{}", x);
 }

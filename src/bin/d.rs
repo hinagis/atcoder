c8e18afb8a39ee19fff2fb7c68796e5645e7f024
hinagis <hasevent@gmail.com>
2020-12-13 22:49:54 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -17,7 +17,7 @@ fn main() {
         }
         for i in 1..m {
             if a[i] - a[i - 1] > 1 {
-                min = min.min(a[i] - a[i - 1]);
+                min = min.min(a[i] - a[i - 1] - 1);
             }
         }
         if n - a[m - 1] > 0 {
@@ -30,14 +30,23 @@ fn main() {
             let mut c = 0;
             if a[0] > 1 {
                 c += (a[0] - 1) / min;
+                if (a[0] - 1) % min != 0 {
+                    c += 1;
+                }
             }
             for i in 1..m {
                 if a[i] - a[i - 1] > 1 {
-                    c += (a[i] - a[i - 1]) / min;
+                    c += (a[i] - a[i - 1] - 1) / min;
+                    if (a[i] - a[i - 1] - 1) % min != 0 {
+                        c += 1;
+                    }
                 }
             }
             if n - a[m - 1] > 0 {
-                c += (n - a[m - 1] - 1) / min;
+                c += (n - a[m - 1]) / min;
+                if (n - a[m - 1]) % min != 0 {
+                    c += 1;
+                }
             }
             println!("{}", c);
         }

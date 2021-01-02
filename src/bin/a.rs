abe003121048fccaf4e0af03131fc7bdee88534d
hinagis <hasevent@gmail.com>
2021-01-02 22:50:46 +0900

    [end of contest] a to d are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,12 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input, marker::Chars};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! {
+        a: Chars,
+        b: Chars,
+    }
+    let a = a.iter().fold(0, |s, &e| s + e as u32 - '0' as u32);
+    let b = b.iter().fold(0, |s, &e| s + e as u32 - '0' as u32);
+
+    println!("{}", a.max(b));
 }

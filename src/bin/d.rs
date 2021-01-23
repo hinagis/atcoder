0bc7374adbc0fffea47c343b8ebbe82b0ce73ffa
hinagis <hasevent@gmail.com>
2021-01-23 22:46:13 +0900

    d is missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,16 @@
 fn main() {
     proconio::input! {
         n: usize,
+        s: [String; n]
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", calc(&s, n));
+}
+
+fn calc(s: &Vec<String>, i: usize) -> u32 {
+    if i == 0 {
+        1
+    } else {
+        calc(s, i - 1) + if s[i - 1] == "OR" {1 << (i as u32)} else {0}
+    }
 }

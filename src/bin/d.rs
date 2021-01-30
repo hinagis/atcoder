8d0a4f3f6c0db3167a3b7925bc1f8d649a626045
hinagis <hasevent@gmail.com>
2021-01-30 22:43:52 +0900

    d is miss

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,16 @@
 fn main() {
     proconio::input! {
-        n: usize,
+        n: i64,
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut r = 0;
+    for i in 1..=n {
+        if (2 * n - i * (i - 1)) % (2 * i) == 0 {
+            r += 1;
+        }
+        if 2 * n <= i * (i - 1) {
+            break;
+        }
+    }
+    println!("{}", r * 2);
 }

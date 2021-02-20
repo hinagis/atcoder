44bfbaebac9afab4e62bdb278643ecbbc36c1445
hinagis <hasevent@gmail.com>
2021-02-20 23:47:33 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,25 +1,29 @@
-use proconio::{input, marker::Chars};
+use proconio::{input, marker::Bytes};
 
 fn main() {
     input! {
-        x: Chars,
-        m: u128,
+        x: Bytes,
+        m: u64,
     }
-    let bb = x.iter().fold(0, |m, &v| (v as u128 - '0' as u128).max(m)) + 1;
-    let mut b = 2u128.pow((format!("{:b}", m).len() / x.len()).max(1) as u32 - 1).max(bb);
-    while calc(&x, b) <= m {
-        b += 1;
-    }
-    println!("{}", b - bb);
-}
+    let x = x.iter().map(|d| (d - b'0') as u64).collect::<Vec<_>>();
 
-fn calc(x: &Vec<char>, b: u128) -> u128 {
-    let mut n = 0;
-    let mut k = 1;
-    for i in 0..x.len() {
-        let d = x[x.len() - i - 1] as u128 - '0' as u128;
-        n += d * k;
-        k *= b;
+    if x.len() == 1 {
+        println!("{}", if x[0] <= m {'1'} else {'0'});
+    } else {
+        let b = x.iter().fold(0, |m, &v| v.max(m));
+        let calc = |b| x.iter().fold(0u64, |n, &d| n.saturating_mul(b).saturating_add(d));
+        let check = |b| calc(b) <= m;
+        let binsearch = |mut l, mut r| {
+            while l + 1 < r {
+                let b = (l + r) / 2;
+                if check(b) {
+                    l = b;
+                } else {
+                    r = b;
+                }
+            }
+            l
+        };
+        println!("{}", binsearch(b, m + 1) - b);
     }
-    n
 }

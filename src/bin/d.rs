226061bd6474063496acb4abc7eb27ee11cca660
hinagis <hasevent@gmail.com>
2021-04-24 22:48:05 +0900

    d is missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,23 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input, marker::Usize1};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(Usize1, Usize1); m]
+    }
+
+    let mut c = vec![3u64; n];
+    for i in 0..n {
+        for j in 0..m {
+            let (a, b) = ab[j];
+            let (a, b) = if b == i {(b, a)} else {(a, b)};
+            if a == i {
+                if b > i {
+                    c[b] = c[b].saturating_sub(1);
+                }
+            }
+        }
+    }
+    println!("{}", c.iter().fold(1, |r, c| r * c))
 }

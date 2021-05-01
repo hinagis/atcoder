3f6d68eca0ef7c72c9a21e00d3eb35b72a69fd1e
hinagis <hasevent@gmail.com>
2021-05-01 23:25:53 +0900

    d is refactored

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -4,23 +4,23 @@ use proconio::{input, fastout, marker::Chars};
 fn main() {
     input! {s: Chars}
 
-    let mut t = Vec::with_capacity(5 * 100_000);
+    let mut t = std::collections::VecDeque::new();
     let mut f = false;
     for i in 0..s.len() {
         if s[i] == 'R' {
             f ^= true;
         } else {
             if f {
-                if t.len() == 0 || t[0] != s[i] {
-                    t.insert(0, s[i]);
+                if t.len() == 0 || *t.front().unwrap() != s[i] {
+                    t.push_front(s[i]);
                 } else {
-                    t.remove(0);
+                    t.pop_front();
                 }
             } else {
-                if t.len() == 0 || t[t.len() - 1] != s[i] {
-                    t.push(s[i]);
+                if t.len() == 0 || *t.back().unwrap() != s[i] {
+                    t.push_back(s[i]);
                 } else {
-                    t.remove(t.len() - 1);
+                    t.pop_back();
                 }
             }
         }

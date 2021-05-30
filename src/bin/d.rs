e0ba563a3dd79834177d0cb621fe0d94c4c58101
hinagis <hasevent@gmail.com>
2021-05-30 23:14:34 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -2,22 +2,41 @@ fn main() {
     proconio::input! {
         n: usize,
         k: usize,
-        a: [[u32; n]; n]
+        a: [[i32; n]; n],
     }
-    let mut m = u32::max_value();
-    let mut kk = vec![0; k * k];
-    let mi = kk.len() - 1 - kk.len() / 2;
-    for i in 0..=(n - k) {
-        for j in 0..=(n - k) {
-            for y in 0..k {
-                for x in 0..k {
-                    kk[y * k + x] = a[i + y][j + x];
+
+    let lim = ((k * k) / 2) + 1;
+    let mut s = vec![vec![0; n + 1]; n + 1];
+    let mut ng = -1;
+    let mut ok = 1_000_000_000;
+    while ng + 1 < ok {
+        let mid = (ng + ok) / 2;
+        for i in 0..n {
+            for j in 0..n {
+                s[i + 1][j + 1] = s[i + 1][j] + s[i][j + 1] - s[i][j];
+                if a[i][j] > mid {
+                    s[i + 1][j + 1] += 1;
                 }
             }
-            kk.sort();
-            m = m.min(kk[mi]);
+        }
+
+        if chk(&s, n, k, lim) {
+            ok = mid;
+        } else {
+            ng = mid;
         }
     }
 
-    println!("{}", m);
+    println!("{}", ok);
+}
+
+fn chk(s: &Vec<Vec<usize>>, n: usize, k: usize, lim: usize) -> bool {
+    for i in 0..(n - k + 1) {
+        for j in 0..(n - k + 1) {
+            if s[i + k][j + k] + s[i][j] - s[i][j + k] - s[i + k][j] < lim {
+                return true;
+            }
+        }
+    }
+    false
 }

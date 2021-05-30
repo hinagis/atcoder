7f1d80dc0a76473a0383ce8dc2e6694f363a4304
hinagis <hasevent@gmail.com>
2021-05-30 23:13:54 +0900

    d is missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,23 @@
 fn main() {
     proconio::input! {
         n: usize,
+        k: usize,
+        a: [[u32; n]; n]
     }
-    let r = n;
+    let mut m = u32::max_value();
+    let mut kk = vec![0; k * k];
+    let mi = kk.len() - 1 - kk.len() / 2;
+    for i in 0..=(n - k) {
+        for j in 0..=(n - k) {
+            for y in 0..k {
+                for x in 0..k {
+                    kk[y * k + x] = a[i + y][j + x];
+                }
+            }
+            kk.sort();
+            m = m.min(kk[mi]);
+        }
+    }
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", m);
 }

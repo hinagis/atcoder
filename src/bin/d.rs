84285925534e0a8e31ed2739a272e1796c6f2526
hinagis <hasevent@gmail.com>
2021-06-19 23:43:53 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,27 +1,47 @@
+const L: usize = 200001;
+
 fn main() {
     proconio::input! {
         n: usize,
         a: [usize; n]
     }
-    let mut t = std::collections::HashMap::new();
-    for i in 0..n / 2 {
-        if a[i] != a[n - i - 1] {
-            let (a0, a1) = (a[i].min(a[n - i - 1]), a[i].max(a[n - i - 1]));
-            t.entry(a0).or_insert(std::collections::HashSet::new()).insert(a1);
+
+    let mut c = 0;
+
+    let mut f = [false; L];
+    for i in 0..n {
+        if f[a[i]] == false {
+            f[a[i]] = true;
+            c += 1;
         }
     }
-    let mut c = 0;
-    while ! t.is_empty() {
-        c += 1;
-        let a = *t.keys().last().unwrap();
-        let mut e = t.remove(&a).unwrap();
-        let b = *e.iter().last().unwrap();
-        e.remove(&b);
-        for v in e.iter() {
-            let (a0, a1) = (b.min(*v), b.max(*v));
-            t.entry(a0).or_insert(std::collections::HashSet::new()).insert(a1);
+
+    let mut g = vec![vec![]; L];
+    let mut p = 0;
+    let mut q = n - 1;
+    while p < q  {
+        g[a[p]].push(a[q]);
+        g[a[q]].push(a[p]);
+        p += 1;
+        q -= 1;
+    }
+
+    for i in 1..L {
+        if f[i] {
+            c -= 1;
+            dfs(&mut f, &g, i);
         }
     }
 
     println!("{}", c);
+}
+
+fn dfs(f: &mut [bool], g: &Vec<Vec<usize>>, i: usize) {
+    if f[i] == false {
+        return;
+    }
+    f[i] = false;
+    for &j in &g[i] {
+        dfs(f, g, j);
+    }
 }

e3eed6948c2983a0d8f8b5ef22aba4b5cafe25ed
hinagis <hasevent@gmail.com>
2021-07-04 23:28:33 +0900

    d is still missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -15,9 +15,7 @@ fn main() {
     let mut kts = vec![vec![vec![0; n + 1]; n + 1]; n + 1];
     for k in 1..=n {
         for t in 1..=n {
-            let mut f = vec![false; n + 1];
-            f[t] = true;
-            dfs(&d, &mut kts[k], &mut f, k, t, t, 0);
+            dfs(&d, &mut kts[k], k, t, t, 0);
         }
     }
 
@@ -29,18 +27,18 @@ fn main() {
             }
         }
     }
-    dbg!(&d);
     println!("{}", r);
 }
 
-fn dfs(d: &Vec<HashMap<usize, u64>>, ts: &mut Vec<Vec<u64>>, f: &mut Vec<bool>, k: usize, t: usize, b: usize, c: u64) {
+fn dfs(d: &Vec<HashMap<usize, u64>>, ts: &mut Vec<Vec<u64>>, k: usize, t: usize, b: usize, c: u64) {
     for &a in d[b].keys() {
         let c = c + d[b].get(&a).unwrap();
-        ts[t][a] = if ts[t][a] == 0 {c} else {ts[t][a].min(c)};
-        if ! f[a] {
-            f[a] = true;
-            if a <= k {
-                dfs(d, ts, f, k, t, a, c);
+        if a != t {
+            if ts[t][a] == 0 || ts[t][a] > c {
+                ts[t][a] = if ts[t][a] == 0 {c} else {ts[t][a].min(c)};
+                if a <= k {
+                    dfs(d, ts, k, t, a, c);
+                }
             }
         }
     }

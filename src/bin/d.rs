07f82e690c5372f0ecaf4936cf395eb7d66630fe
hinagis <hasevent@gmail.com>
2021-07-04 23:41:44 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,45 +1,32 @@
-use std::collections::HashMap;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    proconio::input! {
+    input! {
         n: usize,
         m: usize,
-        abc: [(usize, usize, u64); m]
-    }
-    let mut d = vec![HashMap::new(); n + 1];
-    for i in 0..m {
-        let (a, b, c) = abc[i];
-        d[b].insert(a, c);
+        abc: [(Usize1, Usize1, u64); m]
     }
 
-    let mut kts = vec![vec![vec![0; n + 1]; n + 1]; n + 1];
-    for k in 1..=n {
-        for t in 1..=n {
-            dfs(&d, &mut kts[k], k, t, t, 0);
-        }
+    let mut d = vec![vec![1 << 60; n]; n];
+    for i in 0..n {
+        d[i][i] = 0;
+    }
+    for &(a, b, c) in &abc {
+        d[a][b] = c
     }
 
     let mut r = 0;
-    for k in 1..=n {
-        for t in 1..=n {
-            for s in 1..=n {
-                r += kts[k][t][s];
-            }
-        }
-    }
-    println!("{}", r);
-}
-
-fn dfs(d: &Vec<HashMap<usize, u64>>, ts: &mut Vec<Vec<u64>>, k: usize, t: usize, b: usize, c: u64) {
-    for &a in d[b].keys() {
-        let c = c + d[b].get(&a).unwrap();
-        if a != t {
-            if ts[t][a] == 0 || ts[t][a] > c {
-                ts[t][a] = if ts[t][a] == 0 {c} else {ts[t][a].min(c)};
-                if a <= k {
-                    dfs(d, ts, k, t, a, c);
+    for k in 0..n {
+        let mut nxt = vec![vec![0; n]; n];
+        for i in 0..n {
+            for j in 0..n {
+                nxt[i][j] = d[i][j].min(d[i][k] + d[k][j]);
+                if nxt[i][j] < 1 << 59 {
+                    r += nxt[i][j]
                 }
             }
         }
+        d = nxt
     }
+    println!("{}", r);
 }

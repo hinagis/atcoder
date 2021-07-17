2306f42c6e641fc01084418c7d7e633905b86e23
hinagis <hasevent@gmail.com>
2021-07-17 23:47:37 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -2,43 +2,33 @@ fn main() {
     proconio::input! {
         h: usize,
         w: usize,
-        c: i64,
-        a: [[i64; w]; h]
+        c: u64,
+        mut a: [[u64; w]; h]
     }
-    let mut b = vec![(0, 0, 0); h * w];
-    for i in 0..h {
-        for j in 0..w {
-            b[i * w + j] = (a[i][j], i, j);
+
+    let mut r = u64::max_value();
+    for _ in 0..2 {
+        let mut dp = vec![vec![0; w]; h];
+        dp[0][0] = a[0][0];
+        for i in 1..h {
+            dp[i][0] = a[i][0].min(dp[i - 1][0] + c);
+            r = r.min(a[i][0] + dp[i - 1][0] + c);
         }
-    }
-    b.sort();
-    let mut r = b[0].0 + b[1].0 + c * ((b[0].1 as i64 - b[1].1 as i64).abs() + (b[0].2 as i64 - b[1].2 as i64).abs());
-    for i in 0..10000.min(h * w) {
-        for j in i + 1..100000.min(h * w) {
-            r = r.min(b[i].0 + b[j].0 + c * ((b[i].1 as i64 - b[j].1 as i64).abs() + (b[i].2 as i64 - b[j].2 as i64).abs()));
+        for j in 1..w {
+            dp[0][j] = a[0][j].min(dp[0][j - 1] + c);
+            r = r.min(a[0][j] + dp[0][j - 1] + c);
         }
-    }
-    /*
-    let mut r = a[0][0] + a[0][1] + c;
-    for i in 0..h {
-        for j in 0..w {
-            for x in j + 1..w {
-                r = r.min(a[i][j] + a[i][x] + c * ((x - j) as u64));
-            }
-            for y in i + 1..h {
-                r = r.min(a[i][j] + a[y][j] + c * ((y - i) as u64));
-                for x in 0..j {
-                    r = r.min(a[i][j] + a[y][x] + c * (((y - i) + (j - x)) as u64));
-                }
-            }
-            for y in i + 1..h {
-                for x in j + 1..w {
-                    r = r.min(a[i][j] + a[y][x] + c * (((y - i) + (x - j)) as u64));
-                }
+        for i in 1..h {
+            for j in 1..w {
+                dp[i][j] = a[i][j].min(dp[i][j - 1].min(dp[i - 1][j]) + c);
+                r = r.min(a[i][j] + dp[i][j - 1].min(dp[i - 1][j]) + c);
             }
         }
+    
+        for i in 0..h {
+            a[i].reverse();
+        }
     }
-    */
 
     println!("{}", r);
 }

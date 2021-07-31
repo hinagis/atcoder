7ec8d0de3626b04b38e58ab49d218a0069a4d835
hinagis <hasevent@gmail.com>
2021-07-31 22:56:48 +0900

    e is cleared

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -5,30 +5,23 @@ fn main() {
         n: usize, m: usize, k: usize,
         uv: [(usize, usize); m]
     }
-    let mut e = vec![vec![true; n + 1]; n + 1];
-    for i in 1..=n {
-        e[i][i] = false;
-    }
+    let mut e = vec![vec![]; n + 1];
     for &(u, v) in &uv {
-        e[u][v] = false;
-        e[v][u] = false;
+        e[u].push(v);
+        e[v].push(u);
     }
 
     let mut dp = vec![vec![0; n + 1]; k + 1];
     dp[0][1] = 1;
-    let mut q = std::collections::HashSet::new();
-    q.insert(1);
     for k in 0..k {
-        let mut nq = std::collections::HashSet::new();
-        for &i in &q {
-            for j in 1..=n {
-                if e[i][j] {
-                    dp[k + 1][j] = (dp[k + 1][j] + dp[k][i]) % M;
-                    nq.insert(j);
-                }
+        let s = dp[k].iter().fold(0, |s, c| s + c);
+        for i in 1..=n {
+            dp[k + 1][i] = s - dp[k][i];
+            for &j in &e[i] {
+                dp[k + 1][i] -= dp[k][j];
             }
+            dp[k + 1][i] %= M;
         }
-        q = nq;
     }
     println!("{}", dp[k][1]);
 }

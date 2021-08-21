e0bf4a5bfca1ee70c532bfd730b62ba013309e5a
hinagis <hasevent@gmail.com>
2021-08-21 23:22:55 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,49 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input, fastout};
 
-    println!("Yes");
-    println!("{}", r);
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n]
+    }
+    let mut e = vec![true; 100001];
+    for i in 0..n {
+        let v = pfact(a[i]);
+        for &j in &v {
+            if e[j] {
+                for k in (j..100001).step_by(j) {
+                    e[k] = false;
+                }
+            }
+        }
+    }
+    let mut c = 0;
+    for i in 1..=m {
+        if e[i] {
+            c += 1;
+        }
+    }
+    println!("{}", c);
+    for i in 1..=m {
+        if e[i] {
+            println!("{}", i);
+        }
+    }
+}
+
+fn pfact(mut m: usize) -> Vec<usize> {
+    let mut v = vec![];
+    let mut i = 2;
+    while i * i <= m {
+        while m % i == 0 {
+            m /= i;
+            v.push(i);
+        }
+        i += 1;
+    }
+    if m != 1 {
+        v.push(m)
+    }
+    v
 }

1689b875531556031949e19eb788700e57bafb4f
hinagis <hasevent@gmail.com>
2021-12-30 15:43:33 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,18 @@
 fn main() {
     proconio::input! {
         n: usize,
+        k: i64,
+        a: [i64; n]
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut c = 0u64;
+    let mut h = std::collections::HashMap::new();
+    let mut s = 0;
+    for a in a {
+        *h.entry(s).or_insert(0) += 1;
+        s += a;
+        c += *h.get(&(s - k)).unwrap_or(&0);
+    }
+
+    println!("{}", c);
 }

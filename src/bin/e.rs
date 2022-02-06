9ab4a3ac913040d9d2ac0757f2bfdd9328195949
hinagis <hasevent@gmail.com>
2022-02-06 12:25:43 +0900

    e and f are cleared

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,9 +1,16 @@
+use petgraph::unionfind::UnionFind;
+
 fn main() {
     proconio::input! {
         n: usize,
+        q: usize,
+        lr: [(usize, usize); q],
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut g = UnionFind::new(n + 1);
+    for &(l, r) in &lr {
+        g.union(l - 1, r);
+    }
+
+    println!("{}", if g.find(0) == g.find(n) {"Yes"} else {"No"});
 }
--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,9 +1,35 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, marker::Usize1 as U1};
+const M: u64 = 998244353;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {n: usize, k: usize}
+
+    let a = (|| {
+        let mut a = vec![0; n];
+        I! {p: [U1; n]}
+        for &p in &p {
+            I!{q: U1}
+            a[p] = q;
+        }
+        a
+    })();
+
+    let mut dp = vec![vec![0; n + 1]; k + 1];
+    dp[0][n] = 1;
+    for i in 0..n {
+        let mut t = vec![vec![0; n + 1]; k + 1];
+        for u in 0..=k {
+            for v in 0..=n {
+                if u < k && a[i] < v {
+                    t[u + 1][v] += dp[u][v];
+                    t[u + 1][v] %= M;
+                }
+                t[u][v.min(a[i])] += dp[u][v];
+                t[u][v.min(a[i])] %= M;
+            }
+        }
+        std::mem::swap(&mut dp, &mut t);
+    }
+
+    println!("{}", dp[k].iter().fold(0, |s, v| (s + v) % M));
 }

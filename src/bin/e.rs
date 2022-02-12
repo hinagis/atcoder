b9f38f6dc91185cb45e15a93a52a0e9247f766f1
hinagis <hasevent@gmail.com>
2022-02-12 11:13:18 +0900

    e is updated

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,40 +1,24 @@
-use proconio::{input as I, marker::Usize1 as U1};
+use petgraph::{algo::dijkstra, prelude::*};
+use proconio::{input, marker::Usize1 as U1};
 
 fn main() {
-    I! {
+    input! {
         n: usize,
         m: usize,
-        h: [i64; n]
+        h: [i64; n],
+        uv: [(U1, U1); m],
     }
 
-    let mut g = vec![vec![]; n];
-    for _ in 0..m {
-        I! {u: U1, v: U1}
-        g[u].push((v, (h[v] - h[u]).max(0)));
-        g[v].push((u, (h[u] - h[v]).max(0)));
-    }
+    let mut g: UnGraph<i64, ()> = Graph::new_undirected();
+    let p: Vec<_> = h.iter().map(|&h| g.add_node(h)).collect();
+    g.extend_with_edges(uv.iter().map(|&(u, v)| (p[u], p[v])));
 
-    let mut q = std::collections::BinaryHeap::new();
-    q.push((0, 0));
-    let mut p = vec![std::i64::MAX; n];
-    p[0] = 0;
-
-    while let Some((d, u)) = q.pop() {
-        if p[u] >= -d {
-            for &(v, c) in &g[u] {
-                if p[v] > p[u] + c {
-                    p[v] = p[u] + c;
-                    q.push((-p[v], v));
-                }
-            }
-        }
-    }
-
-    println!("{}", p
-        .iter()
-        .enumerate()
-        .map(|(i, &x)| -x - h[i] + h[0])
-        .max()
+    println!("{}", h[0] - dijkstra(&g, p[0], None, |e| {
+            (g[e.target()] - g[e.source()]).max(0)
+        })
+        .into_iter()
+        .map(|(n, c)| g[n] + c)
+        .min()
         .unwrap()
     );
 }

c558657d378cc57c233663f9c274fd32d5398440
hinagis <hasevent@gmail.com>
2022-02-26 22:56:07 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,4 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
-
-    println!("Yes");
-    println!("{}", r);
+    proconio::input! {a: [usize; 10]}
+    println!("{}", a[a[a[0]]]);
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,9 +1,65 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, marker::Chars};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {
+        n: usize,
+        s: [Chars; n]
+    }
+
+    let mut c = vec![vec![[(0, 0); 4]; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == '#' {
+                for k in 0..4 {
+                    c[i][j][k].0 += 1;
+                }
+                for k in 1..6 {
+                    if i + k < n {
+                        c[i + k][j][0].0 += 1;
+                        if j + k < n {
+                            c[i + k][j + k][1].0 += 1;
+                        }
+                        if j >= k {
+                            c[i + k][j - k][2].0 += 1;
+                        }
+                    }
+                    if j + k < n {
+                        c[i][j + k][3].0 += 1;
+                    }
+                }
+            } else {
+                for k in 0..4 {
+                    c[i][j][k].1 += 1;
+                }
+                for k in 1..6 {
+                    if i + k < n {
+                        c[i + k][j][0].1 += 1;
+                        if j + k < n {
+                            c[i + k][j + k][1].1 += 1;
+                        }
+                        if j >= k {
+                            c[i + k][j - k][2].1 += 1;
+                        }
+                    }
+                    if j + k < n {
+                        c[i][j + k][3].1 += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            if c[i][j][0].0 >= 4
+            || (c[i][j][1].0 >= 4 && c[i][j][1].0 + c[i][j][1].1 == 6)
+            || (c[i][j][2].0 >= 4 && c[i][j][2].0 + c[i][j][2].1 == 6)
+            || c[i][j][3].0 >= 4
+            {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }

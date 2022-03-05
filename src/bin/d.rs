39d83db48ca7abbbb497af1c7ac1730401130036
hinagis <hasevent@gmail.com>
2022-03-05 23:05:51 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,21 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, fastout, marker::Bytes};
 
-    println!("Yes");
-    println!("{}", r);
+#[fastout]
+fn main() {
+    I! {s: Bytes, q: usize}
+
+    for _ in 0..q {
+        I! {t: u64, k: u64}
+        println!("{}", calc(&s, t, k - 1) as char);
+    }
+}
+
+fn calc(s: &Vec<u8>, t: u64, k: u64) -> u8 {
+    if t == 0 {
+        s[k as usize]
+    } else if k == 0 {
+        b'A' + (s[0] - b'A' + (t % 3) as u8) % 3
+    } else {
+        b'A' + (calc(s, t - 1, k / 2) - b'A' + (k % 2) as u8 + 1) % 3
+    }
 }

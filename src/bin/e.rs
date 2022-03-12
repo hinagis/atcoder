f836aedd0355843342fac9aa2eb24328feb68af9
hinagis <hasevent@gmail.com>
2022-03-12 22:55:23 +0900

    e is cleared

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,33 +1,25 @@
-use petgraph::algo::dijkstra;
-use petgraph::graph::{NodeIndex, UnGraph};
+use proconio::{input as I, marker::Usize1 as U1};
 
 fn main() {
-    proconio::input! {
-        n: usize,
-        m: usize,
-        e: [(usize, usize, u32); m]
+    I! {
+        n : usize,
+        m : usize,
+        e : [(U1, U1, u32); m]
     }
-    let g = UnGraph::<usize, u32, usize>::from_edges(&e);
-    let mut d = vec![];
-    for i in 0..n {
-        d.push(dijkstra(&g, (i + 1).into(), None, |e| *e.weight()))
-    }
-    let mut r = 0;
+
+    let mut g = vec![vec![1_000_000_000; n]; n];
     for &(a, b, c) in &e {
-        let &w = dijkstra(&g, a.into(), Some(b.into()), |e| *e.weight()).get(&NodeIndex::new(b)).unwrap();
-        if c > w {
-            r += 1
-        } else {
-            if c == w {
-                let mut g = g.clone();
-                g.remove_edge(g.find_edge(NodeIndex::new(a), NodeIndex::new(b)).unwrap());
-                if let Some(&w) = dijkstra(&g, a.into(), Some(b.into()), |e| *e.weight()).get(&NodeIndex::new(b)) {
-                    if c == w {
-                        r += 1
-                    }
-                }
-            }
-        }
+        g[a][b] = c;
+        g[b][a] = c;
     }
-    println!("{}", r);
+
+    let mut d = vec![vec![false; n]; n];
+    for i in 0..n { for j in 0..n { for k in 0..n {
+        if g[j][k] >= g[j][i] + g[i][k] {
+            g[j][k] = g[j][i] + g[i][k];
+            d[j][k] = true;
+        }
+    }}}
+
+    println!("{}", e.iter().filter(|&&(a, b, _)| d[a][b]).count());
 }

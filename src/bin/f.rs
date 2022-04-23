650948959b4d80c925eb3ff3eebcc0ccb86d6cbe
hinagis <hasevent@gmail.com>
2022-04-23 16:27:08 +0900

    f is cleared

--- src/bin/f.rs
+++ src/bin/f.rs
@@ -1,9 +1,33 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, marker::Usize1 as U1};
 
-    println!("Yes");
+fn main() {
+    I! {n: usize, m: usize}
+
+    let mut e = vec![vec![]; n];
+    let mut c = vec![0; n];
+    for _ in 0..m {
+        I! {u: U1, v: U1}
+        e[v].push(u);
+        c[u] += 1;
+    }
+
+    let mut q = std::collections::VecDeque::new();
+    for i in 0..n {
+        if c[i] == 0 {
+            q.push_back(i);
+        }
+    }
+
+    let mut r = n;
+    while let Some(f) = q.pop_front() {
+        r -= 1;
+        for &u in &e[f] {
+            c[u] -= 1;
+            if c[u] == 0 {
+                q.push_back(u);
+            }
+        }
+    }
+
     println!("{}", r);
 }

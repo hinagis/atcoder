e460ff931f02759ca28690a1944b8c0228ebc364
hinagis <hasevent@gmail.com>
2022-04-23 22:47:33 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,8 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
-
-    println!("Yes");
-    println!("{}", r);
+    proconio::input! {x: [u32; 7]}
+    let c = x[6] / (x[0] + x[2]);
+    let t = c * x[0] * x[1] + (x[6] - c * (x[0] + x[2])).min(x[0]) * x[1];
+    let c = x[6] / (x[3] + x[5]);
+    let a = c * x[3] * x[4] + (x[6] - c * (x[3] + x[5])).min(x[3]) * x[4];
+    println!("{}", if t > a {"Takahashi"} else if t < a {"Aoki"} else {"Draw"});
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,9 +1,24 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, marker::Bytes as B};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {
+        n: usize,
+        k: u32,
+        s: [B; n]
+    }
+
+    println!("{}", dfs(&s, vec![0; 26], n, k, 0));
+}
+
+fn dfs(s: &Vec<Vec<u8>>, c: Vec<u32>, n: usize, k: u32, i: usize) -> u32 {
+    let mut m = 0;
+    for i in i..n {
+        let mut c = c.clone();
+        for &s in &s[i] {
+            c[(s - b'a') as usize] += 1;
+        }
+        m = m.max(c.iter().filter(|&&c| c == k).count() as u32);
+        m = m.max(dfs(s, c, n, k, i + 1));
+    }
+    m
 }

d228261a736144169865c0f966c0ec34540afdc6
hinagis <hasevent@gmail.com>
2022-05-08 22:42:43 +0900

    [end of contest] a to d are cleared

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,9 +1,20 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::input as I;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {n: usize, q: usize}
+    let mut a = (1..=n).map(|i| i).collect::<Vec<usize>>();
+    let mut h = std::collections::HashMap::new();
+    for i in 1..=n {
+        h.insert(i, i - 1);
+    }
+    for _ in 0..q {
+        I!{x: usize}
+        let i = *h.get(&x).unwrap();
+        let j = if i == n - 1 {i - 1} else {i + 1};
+        *h.get_mut(&x).unwrap() = j;
+        *h.get_mut(&a[j]).unwrap() = i;
+        a.swap(i, j);
+    }
+
+    println!("{}", a.iter().map(|i| i.to_string()).collect::<Vec<_>>().join(" "));
 }
--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,29 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+    proconio::input! {n: u128}
 
-    println!("Yes");
-    println!("{}", r);
+    let mut p = vec![true; 5000_000];
+    for i in (4..p.len()).step_by(2) {
+        p[i] = false;
+    }
+
+    let mut c = 0;
+    let mut i = 3;
+    while 2 * i * i * i <= n {
+        if p[i as usize] {
+            let mut j = i * 2;
+            while j < p.len() as u128 {
+                p[j as usize] = false;
+                j += i;
+            }
+            let mut j = 2;
+            while j < i && j * i * i * i <= n {
+                if p[j as usize] {
+                    c += 1;
+                }
+                j += 1;
+            }
+        }
+        i += 2;
+    }
+    println!("{}", c);
 }

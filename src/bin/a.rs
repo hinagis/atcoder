724a45c6d66cea8531409fa86ac593a69bb9c6f7
hinagis <hasevent@gmail.com>
2022-05-14 22:47:03 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,4 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
-
-    println!("Yes");
-    println!("{}", r);
+    proconio::input! {s: String}
+    println!("{}", s.repeat(6 / s.len()));
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,9 +1,18 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I};
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {n: usize}
+    let mut m = (0, 0);
+    let mut h = std::collections::HashMap::new();
+    for i in 1..=n {
+        I!{s: String, t: u32}
+        if ! h.contains_key(&s) {
+            h.insert(s, t);
+            if t > m.1 {
+                m = (i, t);
+            }
+        }
+    }
+
+    println!("{}", m.0);
 }

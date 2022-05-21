1f0862ce4e8397780e637c634af4f5742abf127e
hinagis <hasevent@gmail.com>
2022-05-21 22:43:24 +0900

    d is missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,23 @@
 fn main() {
     proconio::input! {
         n: usize,
+        a: [u32; n]
     }
-    let r = n;
-
-    println!("Yes");
+    let mut c = vec![std::collections::HashMap::new(); n];
+    let mut h = std::collections::HashMap::new();
+    for i in (0..n).rev() {
+        *h.entry(a[i]).or_insert(0) += 1;
+        c[i] = h.clone();
+    }
+    let mut r = 0;
+    for i in 0..n - 2 {
+        for j in i + 1..n - 1 {
+            if a[i] != a[j] {
+                let v = c[j].get(&a[i]).unwrap_or(&0);
+                let w = c[j].get(&a[j]).unwrap_or(&0) - 1;
+                r += (n - 1 - j) as u64 - v - w;
+            }
+        }
+    }
     println!("{}", r);
 }

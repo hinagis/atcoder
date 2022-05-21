af7efbe5148deed0622306713ed765a561d552b8
hinagis <hasevent@gmail.com>
2022-05-21 23:03:50 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,23 +1,18 @@
 fn main() {
     proconio::input! {
         n: usize,
-        a: [u32; n]
+        a: [usize; n]
     }
-    let mut c = vec![std::collections::HashMap::new(); n];
-    let mut h = std::collections::HashMap::new();
-    for i in (0..n).rev() {
-        *h.entry(a[i]).or_insert(0) += 1;
-        c[i] = h.clone();
+    let mut c = vec![0; 200_001];
+    for &a in &a {
+        c[a] += 1;
+    }
+    for i in 0..200_000 {
+        c[i + 1] += c[i];
     }
     let mut r = 0;
-    for i in 0..n - 2 {
-        for j in i + 1..n - 1 {
-            if a[i] != a[j] {
-                let v = c[j].get(&a[i]).unwrap_or(&0);
-                let w = c[j].get(&a[j]).unwrap_or(&0) - 1;
-                r += (n - 1 - j) as u64 - v - w;
-            }
-        }
+    for i in 0..n {
+        r += c[a[i] - 1] * (n - c[a[i]]);
     }
     println!("{}", r);
 }

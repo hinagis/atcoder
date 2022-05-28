687492d0244e6f6d558f189d1cc6572b8d87f783
hinagis <hasevent@gmail.com>
2022-05-28 23:09:01 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,26 +1,6 @@
 fn main() {
-    proconio::input! {
-        n: i128,
-        a: i128,
-        b: i128,
-    }
-    let s = n * (1 + n) / 2;
-    println!("{}", s - if a == 1 || b == 1 {
-            s
-        } else if a == b || (a < b && b % a == 0) {
-            let d = n / a;
-            let e = a * d;
-            d * (a + e) / 2
-        } else if b < a && a % b == 0 {
-            let d = n / b;
-            let e = b * d;
-            d * (b + e) / 2
-        } else {
-            let c = a * b;
-            let d = (n / a, n / b, n / c);
-            let e = (a * d.0, b * d.1, c * d.2);
-            let s = (d.0 * (a + e.0) / 2, d.1 * (b + e.1) / 2, d.2 * (c + e.2) / 2);
-            s.0 + s.1 - s.2
-        }
-    );
+    proconio::input! {n: u64, a: u64, b: u64}
+    let f = |x: u64| x * (x + 1) / 2;
+    let c = num::Integer::lcm(&a, &b);
+    println!("{}", f(n) - (a * f(n / a) + b * f(n / b) - c * f(n / c)))
 }

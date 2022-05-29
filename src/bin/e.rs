391aedada35b0d755c955d0996e63a9d1c0e8b7f
hinagis <hasevent@gmail.com>
2022-05-29 11:35:57 +0900

    e is cleared

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,9 +1,27 @@
+const M: u64 = 998244353;
+
 fn main() {
     proconio::input! {
         n: usize,
+        m: usize,
+        k: usize
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut d = vec![1; m];
+    for _ in 1..n {
+        let mut c = d.clone();
+        for i in 1..m {
+            c[i] += c[i - 1];
+        }
+
+        for i in 0..m {
+            d[i] = if i >= k {c[i - k]} else {0};
+            if i + k < m {
+                d[i] += c[m - 1] - c[i + if k > 0 {k - 1} else {0}];
+            }
+            d[i] %= M;
+        }
+    }
+
+    println!("{}", d.iter().sum::<u64>() % M);
 }

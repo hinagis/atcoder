e362cbb4b8ba2858ff2d01e6257885c9f78c95cd
hinagis <hasevent@gmail.com>
2022-06-04 22:44:59 +0900

    [end of contest] a and b are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,4 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
-
-    println!("Yes");
-    println!("{}", r);
+    proconio::input! {n: String}
+    println!("{}", n.chars().collect::<Vec<_>>()[1..].iter().collect::<String>());
 }
--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,9 +1,18 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, fastout as F};
 
-    println!("Yes");
-    println!("{}", r);
+#[F]
+fn main() {
+    I! {n: usize}
+    let mut a = vec![vec![]; n];
+    a[0].push(1);
+    println!("1");
+    for i in 1..n {
+        a[i].push(1);
+        for j in 1..i {
+            let v = a[i - 1][j - 1] + a[i - 1][j];
+            a[i].push(v);
+        }
+        a[i].push(1);
+        println!("{}", a[i].iter().map(|a| a.to_string()).collect::<Vec<_>>().join(" "));
+    }
 }

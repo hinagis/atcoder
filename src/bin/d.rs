734893553e9e0c5ed596a240a2bda32aa870dba3
hinagis <hasevent@gmail.com>
2022-06-05 11:08:05 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,21 @@
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+    proconio::input! {n: usize}
 
-    println!("Yes");
+    let mut r = 0;
+    for i in 1..=n {
+        let mut k = i;
+        let mut j = 2;
+        while j * j <= k {
+            while k % (j * j) == 0 {
+                k /= j * j;
+            }
+            j += 1;
+        }
+        let mut j = 1;
+        while k * j * j <= n {
+            r += 1;
+            j += 1;
+        }
+    }
     println!("{}", r);
 }

e3851be4ad1012376f0dd787f2f4560363fa64c0
hinagis <hasevent@gmail.com>
2022-06-18 22:50:13 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,24 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::{input as I, fastout as F};
 
-    println!("Yes");
-    println!("{}", r);
+#[F]
+fn main() {
+    I! {n: usize}
+
+    let mut a = [0; 200_002];
+    for _ in 0..n {
+        I! {l: usize, r: usize}
+        a[l] += 1;
+        a[r] -= 1;
+    }
+    for i in 1..=200_001 {
+        a[i] += a[i - 1]
+    }
+    for i in 1..=200_001 {
+        if a[i - 1] == 0 && a[i] != 0 {
+            print!("{} ", i)
+        }
+        if a[i - 1] != 0 && a[i] == 0 {
+            println!("{}", i)
+        }
+    }
 }

be19e0f4aa40ffa03a3debc87aae458914f1058c
hinagis <hasevent@gmail.com>
2022-06-18 22:50:04 +0900

    [end of contest] a to c are cleared

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,9 +1,24 @@
 fn main() {
     proconio::input! {
         n: usize,
+        a: [usize; n]
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut p = 0;
+    let mut b = [false; 5];
+    for i in 0..n {
+        b[0] = true;
+        for j in (0..4).rev() {
+            if b[j] {
+                let c = j + a[i];
+                if c < 4 {
+                    b[c] = true;
+                } else {
+                    p += 1;
+                }
+                b[j] = false;
+            }
+        }
+    }
+    println!("{}", p);
 }

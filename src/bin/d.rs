3edb2f4fadd7d376f657a4bd50b2bda3a172dad5
hinagis <hasevent@gmail.com>
2022-08-22 12:17:06 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,28 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use proconio::input as I;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    I! {
+        n: usize,
+        p: u64, q: u64, r: u64,
+    }
+
+    let mut h = std::collections::HashSet::with_capacity(n);
+    h.insert(0);
+    let mut s = 0;
+    for _ in 0..n {
+        I! {a: u64}
+        s += a;
+        h.insert(s);
+    }
+
+    for &x in &h {
+        let y = x + p;
+        let z = y + q;
+        let w = z + r;
+        if h.contains(&y) && h.contains(&z) && h.contains(&w) {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }

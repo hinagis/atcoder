d00a056a9b7438ee0a886a1b9bb1464703db875e
hinagis <hasevent@gmail.com>
2022-09-17 22:58:35 +0900

    [end of contst] a to d are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,9 +1,11 @@
 fn main() {
     proconio::input! {
-        n: usize,
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    println!("{}", (a + b) * (c - d));
+    println!("Takahashi");
 }
--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,27 @@
+use petgraph::unionfind::UnionFind as G;
+
 fn main() {
     proconio::input! {
         n: usize,
+        p: [(i32, i32); n]
     }
-    let r = n;
 
-    println!("Yes");
-    println!("{}", r);
+    let mut g = G::new(n);
+
+    for i in 0..n {
+        let (x, y) = p[i];
+        for j in 0..n {
+            let (u, v) = p[j];
+            if (x - 1 == u && (y - 1 == v || y     == v))
+            || (x     == u && (y - 1 == v || y + 1 == v))
+            || (x + 1 == u && (y     == v || y + 1 == v)) {
+                g.union(i, j);
+            }
+        }
+    }
+    let mut h = std::collections::HashSet::new();
+    for i in 0..n {
+        h.insert(g.find(i));
+    }
+    println!("{}", h.len());
 }

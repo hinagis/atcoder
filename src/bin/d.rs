ce5efdab5dd1d7b7af1bd571076c53cd9d22b04f
hinagis <hasevent@gmail.com>
2022-12-03 22:57:37 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,9 +1,16 @@
-fn main() {
-    proconio::input! {
-        n: usize,
-    }
-    let r = n;
+use num_integer::gcd;
 
-    println!("Yes");
-    println!("{}", r);
+fn main() {
+    proconio::input! {k: u64}
+    let mut n = k;
+    let mut x = 2;
+    while x * x <= k {
+        n /= gcd(n, x);
+        if n == 1 {
+            println!("{}", x);
+            return;
+        }
+        x += 1;
+    }
+    println!("{}", ((x - 1 + n) / n) * n);
 }

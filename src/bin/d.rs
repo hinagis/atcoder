e966910639e5c1a2a76fe3c4137617751beb8eb2
hinagis <hasevent@gmail.com>
2022-12-29 17:22:09 +0900

    d is refactored

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,27 +1,21 @@
 fn main() {
-    proconio::input! {
+    proconio::input!{
         n: usize,
         k: usize,
         d: usize,
         a: [usize; n],
-    }
-    let mut dp = vec![vec![vec![-1i64; d]; k + 1]; n + 1];
-    dp[0][0][0] = 0;
-    for i in 0..n {
-        for j in 0..=k {
-            for m in 0..d {
-                let v = dp[i][j][m];
-                if v == -1 {
-                    continue;
-                }
-                let p = &mut dp[i + 1][j][m];
-                *p = (*p).max(v);
-                if j != k {
-                    let p = &mut dp[i + 1][j + 1][(m + a[i]) % d];
-                    *p = (*p).max(v + a[i] as i64);
-                }
+    };
+    let mut dp = vec![vec![-1i64; d]; k + 1];
+    dp[0][0] = 0;
+    for a in a {
+        for i in (0..k).rev() {
+            for j in 0..d {
+                let v = dp[i][j];
+                if v < 0 {continue}
+                let p = &mut dp[i + 1][(j + a) % d];
+                *p = (*p).max(v + a as i64);
             }
         }
     }
-    println!("{}", dp[n][k][0]);
+    println!("{}", dp[k][0]);
 }

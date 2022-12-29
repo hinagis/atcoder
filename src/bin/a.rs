867e4f4f752cb8e2f79928b44ddb883d8bd30078
hinagis <hasevent@gmail.com>
2022-12-29 17:09:17 +0900

    a to d are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,6 +1,9 @@
+use proconio::{input as I, fastout as F};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {n: usize}
+    for i in (0..=n).rev() {
+        println!("{}", i);
     }
-    println!("{}", n);
 }
--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,27 @@
 fn main() {
     proconio::input! {
-        n: usize
+        n: usize,
+        k: usize,
+        d: usize,
+        a: [usize; n],
     }
-    println!("{}", n);
+    let mut dp = vec![vec![vec![-1i64; d]; k + 1]; n + 1];
+    dp[0][0][0] = 0;
+    for i in 0..n {
+        for j in 0..=k {
+            for m in 0..d {
+                let v = dp[i][j][m];
+                if v == -1 {
+                    continue;
+                }
+                let p = &mut dp[i + 1][j][m];
+                *p = (*p).max(v);
+                if j != k {
+                    let p = &mut dp[i + 1][j + 1][(m + a[i]) % d];
+                    *p = (*p).max(v + a[i] as i64);
+                }
+            }
+        }
+    }
+    println!("{}", dp[n][k][0]);
 }

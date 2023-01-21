7dd8b22da6b2e5391fd84a78d50e8ff8c0f95b38
hinagis <hasevent@gmail.com>
2023-01-21 15:40:58 +0900

    a to d are cleared

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,6 +1,17 @@
+use proconio::{input as I, fastout as F, marker::Chars as C};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        s: C
     }
-    println!("{}", n);
+    for i in 1..n {
+        for k in 0..n {
+            if k + i >= n || s[k] == s[k + i] {
+                println!("{}", k);
+                break;
+            }
+        }
+    }
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,6 +1,12 @@
+use proconio::{input as I, marker::Bytes as B};
+
 fn main() {
-    proconio::input! {
-        n: usize
-    }
-    println!("{}", n);
+    I! {s: B}
+    println!("{}",
+        s.iter()
+         .rev()
+         .enumerate()
+         .map(|(i, c)| (c - b'@') as u64 * 26u64.pow(i as u32))
+         .sum::<u64>()
+    );
 }

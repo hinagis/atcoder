fa61ad2a5fb33bf108c6fee0f6657f8cd521bf78
hinagis <hasevent@gmail.com>
2023-01-21 23:12:15 +0900

    c, e are cleared

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,6 +1,22 @@
+use proconio::{input as I, marker::Chars as C};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        a: u64,
+        b: u64,
+        s: C
     }
-    println!("{}", n);
+
+    let mut r = std::u64::MAX;
+    for i in 0..n {
+        let mut c = a * i as u64;
+        for j in 0..n / 2 {
+            if s[(i + j) % n] != s[(i + n - 1 - j) % n] {
+                c += b;
+            }
+        }
+        r = r.min(c);
+    }
+    println!("{}", r);
 }

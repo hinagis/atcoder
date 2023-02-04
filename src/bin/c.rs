7d3012522df344da8503ece2f70dd562859c66ca
hinagis <hasevent@gmail.com>
2023-02-04 22:44:47 +0900

    c is cleared

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,6 +1,13 @@
+use petgraph::{graph::UnGraph as G, algo::tarjan_scc as S};
+
 fn main() {
     proconio::input! {
-        n: usize
+        _: u32,
+        m: usize,
+        ab: [(u32, u32); m]
     }
-    println!("{}", n);
+
+    let g = G::<(), ()>::from_edges(&ab);
+    let s = S(&g);
+    println!("{}", m - s.iter().fold(0, |s, e| s + e.len() - 1));
 }

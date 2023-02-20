d107462033375c71a57613a85d4d818e183ee368
hinagis <hasevent@gmail.com>
2023-02-20 11:48:47 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,14 @@
+use proconio::{input as I, fastout as F, marker::Usize1 as U};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {t: usize}
+    for _ in 0..t {
+        I! {
+            n: usize,
+            d: usize,
+            k: U
+        }
+        println!("{}", k / (n / num_integer::gcd(d, n)) + k * d % n);
     }
-    println!("{}", n);
 }

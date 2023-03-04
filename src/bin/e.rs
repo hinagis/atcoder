097ba3b429f8fa8e9e364e16c9e8f852a9d7b172
hinagis <hasevent@gmail.com>
2023-03-04 22:42:49 +0900

    e is TLE

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,6 +1,26 @@
+use proconio::{input as I, marker::Usize1 as U};
+use std::collections::{VecDeque as V, HashSet as H};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        e: [(U, U)],
     }
-    println!("{}", n);
+
+    let mut q = V::new();
+    let mut t = vec![H::new(); n];
+    for &(u, v) in &e {
+        t[u].insert(v);
+        q.push_back((u, v));
+    }
+    let mut r = 0;
+    while let Some((u, v)) = q.pop_front() {
+        for j in t[v].clone() {
+            if u != j && t[u].insert(j) {
+                q.push_back((u, j));
+                r += 1;
+            }
+        }
+    }
+    println!("{}", r);
 }

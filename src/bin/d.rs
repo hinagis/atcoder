bc927c9e73cedcf55db63b80e4f3f9df2ecb8938
hinagis <hasevent@gmail.com>
2023-03-11 22:48:27 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,19 @@
+use proconio::{input as I, marker::Usize1 as U};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        e: [(U, char, U, char)],
     }
-    println!("{}", n);
+
+    let mut g = petgraph::unionfind::UnionFind::new(n);
+    let mut x = 0;
+    let mut y = n;
+    for (a, _, c, _) in e {
+        y -= 1;
+        if !g.union(a, c) {
+            x += 1;
+        }
+    }
+    println!("{} {}", x, y);
 }

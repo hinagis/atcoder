fcc1afa66aa44bb214abc2fba9aafa4d7b9efb22
hinagis <hasevent@gmail.com>
2023-03-26 00:26:25 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,15 @@
+use proconio::{input as I, marker::Bytes as B};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {s: B}
+
+    let mut h = std::collections::HashMap::new();
+    h.insert(0, 1u64);
+    let mut c = 0;
+    for &b in &s {
+        c ^= 1 << (b - b'0');
+        *h.entry(c).or_default() += 1;
     }
-    println!("{}", n);
+    
+    println!("{}", h.values().fold(0, |s, v| s + (v * (v - 1)) / 2));
 }

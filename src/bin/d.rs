141d824642f4bbdee2867fdc9945a522fe991952
hinagis <hasevent@gmail.com>
2023-04-01 23:04:35 +0900

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,20 @@
+use std::i64::MAX;
+
 fn main() {
     proconio::input! {
-        n: usize
+        n: i64,
+        m: i64
     }
-    println!("{}", n);
+
+    let mut r = if m <= n {m} else {MAX};
+    if r == MAX {
+        for i in 1..=n {
+            let x = (m + i - 1) / i;
+            if x <= n {
+                r = r.min(i * x);
+            }
+            if i > x {break}
+        }
+    }
+    println!("{}", if r == MAX {-1} else {r});
 }

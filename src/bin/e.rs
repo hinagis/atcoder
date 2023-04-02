ea9185314f0db395edee0e98213c23251b3983ce
hinagis <hasevent@gmail.com>
2023-04-02 12:56:31 +0900

    e is cleared

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,64 +1,13 @@
 use proconio::{input as I, marker::Usize1 as U};
+use petgraph::{Graph as G, Directed as D, algo::tarjan_scc as scc};
 
 fn main() {
-    I! {
-        n: usize,
-        a: [U; n]
-    }
+    I! {a: [U]}
 
-    let mut f = vec![false; n];
-    for &i in &a {
-        f[i] = true;
-    }
-    let mut h = vec![];
-    for i in 0..n {
-        if f[i] {
-            h.push(i);
-        }
-    }
+    let c = a.iter().enumerate().filter(|&(i, &a)| i == a).count();
+    let g = G::<(), (), D, _>::from_edges(
+        a.iter().enumerate().map(|(i, &a)| (i, a))
+    );
 
-    let mut d = vec![false; n];
-    let mut l = vec![false; n];
-    for &i in &h {
-        let mut f = vec![false; n];
-        let mut j = a[i];
-        while ! f[j] {
-            if d[j] || l[j] {break}
-            d[j] = true;
-            f[j] = true;
-            j = a[j];
-        }
-        if ! f[j] || l[j] {continue}
-
-        let s = j;
-        l[s] = true;
-        j = a[j];
-        while j != s {
-            l[j] = true;
-            j = a[j];
-        }
-    }
-
-    for i in 0..n {
-        if d[i] || l[i] {continue}
-
-        let mut f = vec![false; n];
-        let mut j = a[i];
-        while ! f[j] {
-            if l[j] {break}
-            f[j] = true;
-            j = a[j];
-        }
-        if l[j] {continue}
-
-        let s = j;
-        l[s] = true;
-        j = a[j];
-        while j != s {
-            l[j] = true;
-            j = a[j];
-        }
-    }
-
-    println!("{}", l.iter().filter(|&&f| f).count());
+    println!("{}", c + scc(&g).iter().filter(|c| c.len() >= 2).fold(0, |s, c| s + c.len()));
 }

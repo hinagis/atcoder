f597d6362eeaf211b4a0ee44e504805c7912423e
hinagis <hasevent@gmail.com>
2023-04-02 12:04:03 +0900

    e is TLE

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,6 +1,64 @@
+use proconio::{input as I, marker::Usize1 as U};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        a: [U; n]
     }
-    println!("{}", n);
+
+    let mut f = vec![false; n];
+    for &i in &a {
+        f[i] = true;
+    }
+    let mut h = vec![];
+    for i in 0..n {
+        if f[i] {
+            h.push(i);
+        }
+    }
+
+    let mut d = vec![false; n];
+    let mut l = vec![false; n];
+    for &i in &h {
+        let mut f = vec![false; n];
+        let mut j = a[i];
+        while ! f[j] {
+            if d[j] || l[j] {break}
+            d[j] = true;
+            f[j] = true;
+            j = a[j];
+        }
+        if ! f[j] || l[j] {continue}
+
+        let s = j;
+        l[s] = true;
+        j = a[j];
+        while j != s {
+            l[j] = true;
+            j = a[j];
+        }
+    }
+
+    for i in 0..n {
+        if d[i] || l[i] {continue}
+
+        let mut f = vec![false; n];
+        let mut j = a[i];
+        while ! f[j] {
+            if l[j] {break}
+            f[j] = true;
+            j = a[j];
+        }
+        if l[j] {continue}
+
+        let s = j;
+        l[s] = true;
+        j = a[j];
+        while j != s {
+            l[j] = true;
+            j = a[j];
+        }
+    }
+
+    println!("{}", l.iter().filter(|&&f| f).count());
 }

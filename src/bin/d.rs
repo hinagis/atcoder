1b7a0567eaba605cf235c8f8bab16c6ab0c24eaa
hinagis <hasevent@gmail.com>
2023-05-13 22:47:35 +0900

    [end of contest] a to d are cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,27 @@
 fn main() {
     proconio::input! {
+        s: String,
         n: usize
     }
-    println!("{}", n);
+
+    let m = s.len();
+    let mut r = 0;
+    for (i, c) in s.bytes().enumerate() {
+        if c == b'1' {
+            r |= 1 << m - 1 - i;
+        }
+    }
+    if r > n {
+        println!("-1");
+        return;
+    }
+    for (i, c) in s.chars().enumerate() {
+        if c == '?' {
+            let b = r | (1 << m - 1 - i);
+            if b <= n {
+                r = b;
+            }
+        }
+    }
+    println!("{}", r);
 }

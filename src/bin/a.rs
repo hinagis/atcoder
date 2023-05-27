cf412734d22ebbcc0294194dbb9db67c2697f104
hinagis <hasevent@gmail.com>
2023-05-27 22:41:05 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,6 +1,19 @@
+use proconio::{input as I, marker::Chars as C};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        s: C,
+        t: C
     }
-    println!("{}", n);
+    for i in 0..n {
+        if s[i] == t[i] {continue;}
+        if s[i] == '1' && t[i] == 'l' {continue;}
+        if s[i] == 'l' && t[i] == '1' {continue;}
+        if s[i] == '0' && t[i] == 'o' {continue;}
+        if s[i] == 'o' && t[i] == '0' {continue;}
+        println!("No");
+        return;
+    }
+    println!("Yes");
 }
--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,6 +1,37 @@
+use proconio::{input as I, marker::Chars as C};
+
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        _: usize,
+        m: usize,
+        mut h: i32,
+        k: i32,
+        s: C,
+        xy: [(i32, i32); m]
     }
-    println!("{}", n);
+    let mut c = std::collections::HashSet::new();
+    for &e in &xy {
+        c.insert(e);
+    }
+    let mut p = (0, 0);
+    for &a in &s {
+        h -= 1;
+        if h < 0 {
+            println!("No");
+            return;
+        }
+
+        p = match a {
+            'R' => (p.0 + 1, p.1),
+            'L' => (p.0 - 1, p.1),
+            'U' => (p.0, p.1 + 1),
+            'D' => (p.0, p.1 - 1),
+            _ => (0, 0)
+        };
+        if h < k && c.contains(&p) {
+            h = k;
+            c.remove(&p);
+        }
+    }
+    println!("Yes");
 }

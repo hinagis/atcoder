3647b4de40643dfa8933c16a7eb14fe2b9af926a
hinagis <hasevent@gmail.com>
2023-06-03 23:08:30 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,6 +1,18 @@
+use proconio::{input as I, fastout as F};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        h: [(String, u32); n]
     }
-    println!("{}", n);
+    let mut s = 0;
+    for i in 0..n {
+        if h[i].1 < h[s].1 {
+            s = i;
+        }
+    }
+    for i in 0..n {
+        println!("{}", h[(i + s) % n].0);
+    }
 }

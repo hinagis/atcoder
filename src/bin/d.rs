4084ad2c92dd832780b2fd40d001990bdabc26a0
hinagis <hasevent@gmail.com>
2023-06-03 23:09:04 +0900

    d and e are missed

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,51 @@
 fn main() {
     proconio::input! {
-        n: usize
+        _: u32,
+        _: u32,
+        n: usize,
+        mut p: [(u32, u32); n],
+        mut a: [u32],
+        mut b: [u32]
     }
-    println!("{}", n);
+    a.sort();
+    b.sort();
+    p.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut c = vec![vec![]; a.len() + 1];
+    let mut k = 0;
+    for i in 0..a.len() {
+        let mut j = k;
+        while j < n && p[j].0 < a[i] {
+            c[i].push(p[j].1);
+            j += 1;
+        }
+        k = j;
+    }
+    while k < n {
+        c[a.len()].push(p[k].1);
+        k += 1;
+    }
+    for i in 0..a.len() {
+        c[i].sort();
+    }
+    let mut l = std::u32::MAX;
+    let mut r = 0;
+    for i in 0..a.len() {
+        let mut k = 0;
+        for u in 0..b.len() {
+            let j = match c[i].binary_search(&b[u]) {
+                Ok(e) => e - 1,
+                Err(e) => e
+            };
+            let d = (j - k) as u32;
+            l = l.min(d);
+            r = r.max(d);
+            k = j;
+        }
+        let d = (c[i].len() - k) as u32;
+        l = l.min(d);
+        r = r.max(d);
+    }
+
+    println!("{} {}", l, r);
 }
--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,6 +1,23 @@
+use proconio::{input as I, fastout as F, marker::Usize1 as U};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        m: [(U, U)],
+        k: [(U, U)],
+        q: [(U, U)]
     }
-    println!("{}", n);
+    let mut t = petgraph::unionfind::UnionFind::new(n);
+    for &(x, y) in &m {
+        t.union(x, y);
+    }
+
+    let mut h = std::collections::HashSet::new();
+    for &(x, y) in &k {
+        h.insert((t.find(x), t.find(y)));
+    }
+    for &(x, y) in &q {
+        println!("{}", if h.contains(&(t.find(x), t.find(y))) {"No"} else {"Yes"});
+    }
 }

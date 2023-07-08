df85e9eeb500057aa543a09d63e9e047d8e2eb33
hinagis <hasevent@gmail.com>
2023-07-08 22:42:20 +0900

    [end of contest] a to c are cleared

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,6 +1,7 @@
 fn main() {
     proconio::input! {
-        n: usize
+        a: u32,
+        b: u32
     }
-    println!("{}", n);
+    println!("{}", if a % 3 != 0 && a + 1 == b {"Yes"} else {"No"});
 }
--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,6 +1,20 @@
+use proconio::{input as I, fastout as F, marker::Chars as C};
+
+#[F]
 fn main() {
-    proconio::input! {
-        n: usize
+    I! {
+        n: usize,
+        a: [C; n]
     }
-    println!("{}", n);
+    let t = n - 1;
+    let mut b = a.clone();
+    for i in 1..n {
+        b[0][i] = a[0][i - 1];
+        b[t][t - i] = a[t][n - i];
+        b[i][t] = a[i - 1][t];
+        b[t - i][0] = a[n - i][0];
+    }
+    for i in 0..n {
+        println!("{}", b[i].iter().collect::<String>());
+    }
 }

062a2fda954aca94ab8afe5b5bfa7ef1a5d9cf1f
hinagis <hasevent@gmail.com>
2023-09-09 22:45:39 +0900

    [end of contest] a to d are cleared

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,6 +1,62 @@
+use itertools::Itertools;
+
 fn main() {
-    proconio::input! {
-        n: usize
+    proconio::input! {c: [[u32; 3]; 3]}
+    let mut r = 0;
+    for p in (0..9).permutations(9) {
+        let mut x = vec![None; 3];
+        let mut y = vec![None; 3];
+        let mut z = vec![None; 2];
+
+        let mut f = true;
+        for p in p {
+            let i = p / 3;
+            let j = p % 3;
+
+            if let Some(v) = x[j] {
+                if v == c[i][j] {
+                    f = false;
+                    break;
+                }
+                x[j] = None;
+            } else {
+                x[j] = Some(c[i][j]);
+            }
+            if let Some(v) = y[i] {
+                if v == c[i][j] {
+                    f = false;
+                    break;
+                }
+                y[i] = None;
+            } else {
+                y[i] = Some(c[i][j]);
+            }
+            if i == j {
+                if let Some(v) = z[0] {
+                    if v == c[i][j] {
+                        f = false;
+                        break;
+                    }
+                    z[0] = None;
+                } else {
+                    z[0] = Some(c[i][j]);
+                }
+            }
+            if i == 2 - j {
+                if let Some(v) = z[1] {
+                    if v == c[i][j] {
+                        f = false;
+                        break;
+                    }
+                    z[1] = None;
+                } else {
+                    z[1] = Some(c[i][j]);
+                }
+            }
+        }
+        if f {
+            r += 1;
+        }
     }
-    println!("{}", n);
+    println!("{}", r as f64 / 362880f64);
 }
--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,6 +1,36 @@
 fn main() {
     proconio::input! {
-        n: usize
+        n: usize,
+        m: u64,
+        x: [u64; n]
     }
-    println!("{}", n);
+    let mut l = x.iter().fold(0, |s, &x| if x > s {x} else {s});
+    let mut r = 1000_000_000_000_000u64;
+    while l < r {
+        let c = (l + r) / 2;
+        dbg!(l, r, c);
+        if calc(&x, c) > m {
+            l = c + 1;
+        } else {
+            r = c;
+        }
+    }
+
+    println!("{}", r);
+}
+
+fn calc(l: &Vec<u64>, w: u64) -> u64 {
+    let mut r = 1;
+    let mut c = 0;
+    for &l in l {
+        if c == 0 {
+            c = l;
+        } else if c + 1 + l > w {
+            r += 1;
+            c = l;
+        } else {
+            c += 1 + l;
+        }
+    }
+    r
 }

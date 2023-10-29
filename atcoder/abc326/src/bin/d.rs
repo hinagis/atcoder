33b78753b75b3631e298ff7327b6d7ba8c1fcd94
hinagis <hasevent@gmail.com>
2023-10-29 01:46:13 +0000

    d is cleared

--- atcoder/abc326/src/bin/d.rs
+++ atcoder/abc326/src/bin/d.rs
@@ -9,38 +9,23 @@ fn main() {
         c: C
     }
 
-    if r[0] != c[0] {
-        println!("No");
-        return
-    }
-
-    let mut b = vec![vec!['.'; n]; n];
-    b[0][0] = r[0];
-    let mut f = vec![vec![0x7; n + 1]; n + 1];
-    f[0][n] &= !(1 << r[0] as u8 - b'A');
-    f[n][0] &= !(1 << c[0] as u8 - b'A');
-    for i in 0..n {
-        f[i][0] = (1 << r[i] as u8 - b'A') & !(1 << r[0] as u8 - b'A');
-    }
-    for j in 0..n {
-        f[0][j] = (1 << c[j] as u8 - b'A') & !(1 << c[0] as u8 - b'A');
-    }
-
-    if !calc(&r, &c, &b, &f, n, 0, 1) {
+    let b = vec![vec!['.'; n]; n];
+    let f = vec![vec![0x7; n + 1]; n + 1];
+    if !calc(&r, &c, &b, &f, n, 0, 0) {
         println!("No");
     }
 }
 
 fn calc(r: &Vec<char>, c: &Vec<char>,b: &Vec<Vec<char>>, f: &Vec<Vec<u8>>, n: usize, i: usize, mut j: usize) -> bool {
     if i >= n {
-        for i in 1..n {
+        for i in 0..n {
             for j in 0..n {
                 if b[i][j] == r[i] {break}
                 else if b[i][j] == '.' {continue}
                 else {return false}
             }
         }
-        for j in 1..n {
+        for j in 0..n {
             for i in 0..n {
                 if b[i][j] == c[j] {break}
                 else if b[i][j] == '.' {continue}

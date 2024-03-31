c7de8a39e187c51a2c678cebaefa63771d960f30
hinagis <hasevent@gmail.com>
2024-03-31 02:04:19 +0000

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,15 +1,40 @@
 fn main() {
     proconio::input! {
-        a: u64,
-        b: u64,
-        c: u64,
+        a: u128,
+        b: u128,
+        c: u128,
     }
-    let mut d = 0;
-    for i in 0..60 {
-        if c & (1 << i) != 0 {
-            d += 1;
+    let d = c.count_ones() as u128;
+    let f = a.min(d);
+    for i in 0..=f {
+        if a + d - i * 2 == b {
+            let mut j = i;
+            let mut k = a - i;
+            let mut x = 0;
+            let mut i = 0;
+            while j > 0 || k > 0 {
+                if c & (1 << i) == 0 {
+                    if k > 0 {
+                        x |= 1 << i;
+                        k -= 1;
+                    }
+                } else {
+                    if j > 0 {
+                        x |= 1 << i;
+                        j -= 1;
+                    }
+                }
+                i += 1;
+            }
+            let y = c ^ x;
+            if x < 2u128.pow(60) && y < 2u128.pow(60) {
+                println!("{} {}", x, c ^ x);
+            } else {
+                println!("-1");
+            }
+            return;
         }
     }
 
-    println!("{}");
+    println!("-1");
 }

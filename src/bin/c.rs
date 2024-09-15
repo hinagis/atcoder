899c33b8adb948ad7e0b4fa4459d401a75d9a9d3
hinagis <hasevent@gmail.com>
2024-09-15 12:25:45 +0000

    c is cleared

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,43 +1,54 @@
+use std::u64;
+
 use proconio::{input as I, marker::Usize1 as U};
 use itertools::Itertools;
 
 fn main() {
     I! {n: usize}
 
-    let mut g = vec![vec![]; n];
+    let mut g = vec![vec![false; n]; n];
     I! [m: usize];
     for _ in 0..m {
         I! {
             u: U,
             v: U
         }
-        g[u].push(v);
-        g[v].push(u);
+        g[u][v] = true;
+        g[v][u] = true;
     }
 
-    let mut h = vec![vec![]; n];
+    let mut h = vec![vec![false; n]; n];
     I! [m: usize];
     for _ in 0..m {
         I! {
             u: U,
             v: U
         }
-        h[u].push(v);
-        h[v].push(u);
+        h[u][v] = true;
+        h[v][u] = true;
     }
 
     let mut c = vec![vec![0; n]; n];
     for i in 0..n - 1 {
-        I! [a: [U; n - 1 - i]];
-        for j in 0..i {
+        I! [a: [u64; n - 1 - i]];
+        for j in 0..n - 1 - i {
             c[i][i + 1 + j] = a[j];
         }
     }
 
-    
+    let mut m = u64::MAX;
     for p in (0..n).permutations(n) {
-
+        let mut t = 0;
+        for i in 0..n {
+            for j in i + 1..n {
+                let (u, v) = if p[i] < p[j] {(p[i], p[j])} else {(p[j], p[i])};
+                if g[i][j] != h[u][v] {
+                    t += c[u][v];
+                }
+            }
+        }
+        m = m.min(t);
     }
 
-    println!("{}", n);
+    println!("{}", m);
 }

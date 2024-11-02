41c937484b2bde1d336529874709a7a46b15d41f
hinagis <hasevent@gmail.com>
2024-11-02 13:58:52 +0000

    d is cleared

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,8 +1,5 @@
 use proconio::{input as I, marker::Chars as C};
 
-#[derive(PartialEq)]
-enum D {U, D, L, R, N}
-
 fn main() {
     I! {
         h: usize,
@@ -11,69 +8,26 @@ fn main() {
         s: [C; h]
     }
     let mut t = 0u64;
-    let mut c = vec![vec![vec![vec![None; 4]; k]; w]; h];
+    let mut f = vec![vec![true; w]; h];
     for i in 0..h {
         for j in 0..w {
-            let mut f = vec![vec![true; w]; h];
-            t += dfs(&s, &mut f, &mut c, h, w, i, j, k, D::N);
+            t += dfs(&s, &mut f, h, w, i, j, k);
         }
     }
     println!("{}", t);
 }
 
-fn dfs(s: &Vec<Vec<char>>, f: &mut Vec<Vec<bool>>, c: &mut Vec<Vec<Vec<Vec<Option<u64>>>>>, h: usize, w: usize, i: usize, j: usize, k: usize, d: D) -> u64 {
+fn dfs(s: &Vec<Vec<char>>, f: &mut Vec<Vec<bool>>,h: usize, w: usize, i: usize, j: usize, k: usize) -> u64 {
     if s[i][j] == '#' {return 0;}
 
     if k == 0 {return 1;}
     let k = k - 1;
 
     f[i][j] = false;
-
-    let mut t = 0;
-    if d != D::D && i > 0 {
-        t += if let Some(q) = c[i][j][k][0] {
-            q
-        } else if f[i - 1][j] {
-            let q = dfs(s, f, c, h, w, i - 1, j, k, D::U);
-            c[i][j][k][0] = Some(q);
-            q
-        } else {
-            0
-        };
-    }
-    if d != D::U && i < h - 1 {
-        t += if let Some(q) = c[i][j][k][1] {
-            q
-        } else if f[i + 1][j] {
-            let q = dfs(s, f, c, h, w, i + 1, j, k, D::D);
-            c[i][j][k][1] = Some(q);
-            q
-        } else {
-            0
-        };
-    }
-    if d != D::R && j > 0 {
-        t += if let Some(q) = c[i][j][k][2] {
-            q
-        } else if f[i][j - 1] {
-            let q = dfs(s, f, c, h, w, i, j - 1, k, D::L);
-            c[i][j][k][2] = Some(q);
-            q
-        } else {
-            0
-        };
-    }
-    if d != D::L && j < w - 1 {
-        t += if let Some(q) = c[i][j][k][3] {
-            q
-        } else if f[i][j + 1] {
-            let q = dfs(s, f, c, h, w, i, j + 1, k, D::R);
-            c[i][j][k][3] = Some(q);
-            q
-        } else {
-            0
-        };
-    }
+    let t = if i > 0     && f[i - 1][j] {dfs(s, f, h, w, i - 1, j, k)} else {0}
+               + if i < h - 1 && f[i + 1][j] {dfs(s, f, h, w, i + 1, j, k)} else {0}
+               + if j > 0     && f[i][j - 1] {dfs(s, f, h, w, i, j - 1, k)} else {0}
+               + if j < w - 1 && f[i][j + 1] {dfs(s, f, h, w, i, j + 1, k)} else {0};
     f[i][j] = true;
     t
 }

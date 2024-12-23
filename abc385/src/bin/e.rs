6af5bd04ad32959f0f7f445dc40812d2845a20bd
hinagis <hasevent@gmail.com>
2024-12-23 08:58:17 +0000

    e is cleared

--- abc385/src/bin/e.rs
+++ abc385/src/bin/e.rs
@@ -1,12 +1,23 @@
-use proconio::{input as I, fastout as F};
+use proconio::{input as I, marker::Usize1 as U};
 
-#[F]
 fn main() {
-    proconio::input! {
-        n: usize,
+    I! {n: usize}
+    let mut g = vec![vec![]; n];
+    for _ in 1..n {
+        I! {u: U, v: U}
+        g[u].push(v);
+        g[v].push(u);
     }
-    I! {
-        n: usize,
+
+    let l = g.iter().map(|u| u.len()).collect::<Vec<_>>();
+    for i in 0..n {
+        g[i].sort_by(|&a, &b| l[b].cmp(&l[a]));
     }
-    println!("{}", n);
+    let mut m = n;
+    for i in 0..n {
+        for j in 0..l[i] {
+            m = m.min(n - 1 - (j + 1) * (l[g[i][j]]));
+        }
+    }
+    println!("{}", m);
 }

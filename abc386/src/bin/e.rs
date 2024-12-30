6fa912115d8e5ee1a2a1fcf212327f579d18296d
hinagis <hasevent@gmail.com>
2024-12-30 06:06:27 +0000

    e is cleared

--- abc386/src/bin/e.rs
+++ abc386/src/bin/e.rs
@@ -7,8 +7,9 @@ fn main() {
         a: [u64; n]
     }
     let mut m = 0;
+    let (t, k) = if k <= n - k {(0, k)} else {(a.iter().fold(0, |t, a| t ^ a), n - k)};
     for c in (0..n).combinations(k) {
-        let mut t = 0;
+        let mut t = t;
         for i in c {
             t ^= a[i];
         }

2873abe9c5a282313f18ef83f3090f322eb2d504
hinagis <hasevent@gmail.com>
2025-03-01 09:35:06 +0000

    e is cleared

--- abc394/src/bin/e.rs
+++ abc394/src/bin/e.rs
@@ -1,12 +1,30 @@
-use proconio::{input as I, fastout as F};
+use itertools::{iproduct, Itertools};
+use proconio::{input as I, marker::Chars as C};
 
-#[F]
 fn main() {
-    proconio::input! {
-        n: usize,
-    }
     I! {
         n: usize,
+        a: [C; n]
     }
-    println!("{}", n);
+    let mut q = std::collections::VecDeque::new();
+    let mut c = vec![vec![-1; n]; n];
+    for i in 0..n {
+        c[i][i] = 0;
+        q.push_back((i, i));
+    }
+    for i in 0..n {
+        for j in (0..i).chain(i + 1..n) {
+            if a[i][j] == '-' {continue}
+            c[i][j] = 1;
+            q.push_back((i, j));
+        }
+    }
+    while let Some((i, j)) = q.pop_front() {
+        for (k, l) in iproduct!(0..n, 0..n) {
+            if a[k][i] != a[j][l] || a[k][i] == '-' || c[k][l] >= 0 {continue}
+            c[k][l] = c[i][j] + 2;
+            q.push_back((k, l));
+        }
+    }
+    print!("{}", c.iter().map(|c| c.iter().join(" ")).join("\n"));
 }

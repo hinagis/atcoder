746e10952831877451e0b3d0da71c7b2c2fba30f
hinagis <hasevent@gmail.com>
2025-06-07 08:23:36 +0000

    d is cleared

--- abc408/src/bin/d.rs
+++ abc408/src/bin/d.rs
@@ -8,17 +8,17 @@ fn main() {
             n: usize,
             s: C
         }
-        let mut c = vec![];
-        let mut p = '0';
-        let mut l = 0;
-        for i in 0..n {
-            if s[i] != p {
-                c.push(i - l);
-                l = i;
-            }
-        }
-        c.push(n - l);
+        let mut dp = vec![[1000000; 3]; n];
+        dp[0][0] = if s[0] == '0' {0} else {1};
+        dp[0][1] = if s[0] == '1' {0} else {1};
 
-        println!("{}", n);
+        for i in 1..n {
+            dp[i][0] = dp[i - 1][0] + if s[i] == '0' {0} else {1};
+            dp[i][1] = dp[i - 1][0] + if s[i] == '1' {0} else {1};
+            dp[i][1] = dp[i][1].min(dp[i - 1][1] + if s[i] == '1' {0} else {1});
+            dp[i][2] = dp[i][2].min(dp[i - 1][1] + if s[i] == '0' {0} else {1});
+            dp[i][2] = dp[i][2].min(dp[i - 1][2] + if s[i] == '0' {0} else {1});
+        }
+        println!("{}", dp[n - 1].iter().min().unwrap());
     }
 }

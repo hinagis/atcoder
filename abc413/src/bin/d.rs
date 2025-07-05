d54620d2426c1c900f81448ef8fda404b64959b9
hinagis <hasevent@gmail.com>
2025-07-05 13:50:06 +0000

    d is cleared

--- abc413/src/bin/d.rs
+++ abc413/src/bin/d.rs
@@ -12,28 +12,23 @@ fn main() {
             println!("Yes");
             continue;
         }
-        let p = a.iter().filter(|&&e| e > 0).count();
-        let m = a.iter().filter(|&&e| e < 0).count();
+        let p = a.iter().filter(|&&e| e == a[0]).count();
+        if p == n {
+            println!("Yes");
+            continue;
+        }
+        let m = a.iter().filter(|&&e| e == -a[0]).count();
+        if p + m == n && p.min(m) == n / 2 {
+            println!("Yes");
+            continue;
+        }
+
         let mut r = true;
-        if p == 0 || m == 0 {
-            a.sort();
-            for i in 0..n - 2 {
-                if a[i + 1] * a[i + 1] != a[i] * a[i + 2] {
-                    r = false;
-                    break;
-                }
-            }
-        } else if p.max(m) - p.min(m) > 1 {
-            r = false;
-        } else {
-            a.sort_by(|a, b| a.abs().cmp(&b.abs()));
-            for i in 0..n - 2 {
-                if a[i] < 0 && a[i + 1] < 0 || a[i + 1] < 0 && a[i + 2] < 0 ||
-                    a[i] > 0 && a[i + 1] > 0 || a[i + 1] > 0 && a[i + 2] > 0 ||
-                    a[i + 1] * a[i + 1] != a[i] * a[i + 2] {
-                    r = false;
-                    break;
-                }
+        a.sort_by(|a, b| a.abs().cmp(&b.abs()));
+        for i in 0..n - 2 {
+            if a[i + 1] * a[i + 1] != a[i] * a[i + 2] {
+                r = false;
+                break;
             }
         }
         println!("{}", if r {"Yes"} else {"No"});

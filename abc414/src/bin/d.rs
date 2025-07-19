f83fe3f8f4092d093f2b55abd4746d13ba450fb1
hinagis <hasevent@gmail.com>
2025-07-19 07:44:24 +0000

    d is cleared

--- abc414/src/bin/d.rs
+++ abc414/src/bin/d.rs
@@ -1,12 +1,11 @@
-use proconio::{input as I, fastout as F};
+use itertools::Itertools;
 
-#[F]
 fn main() {
     proconio::input! {
         n: usize,
+        m: usize,
+        mut x: [u64; n]
     }
-    I! {
-        n: usize,
-    }
-    println!("{}", n);
+    x.sort();
+    println!("{}", x.windows(2).map(|e| e[1] - e[0]).sorted().take(n - m).sum::<u64>());
 }

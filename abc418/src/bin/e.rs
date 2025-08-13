69ef7dc67ae9995e2c68e0000229d70312151d37
hinagis <hasevent@gmail.com>
2025-08-13 01:55:00 +0000

    e is cleared

--- abc418/src/bin/e.rs
+++ abc418/src/bin/e.rs
@@ -1,12 +1,37 @@
-use proconio::{input as I, fastout as F};
+use num_integer::gcd;
+use std::collections::HashMap as H;
 
-#[F]
 fn main() {
     proconio::input! {
         n: usize,
+        p: [(i64, i64); n]
     }
-    I! {
-        n: usize,
+    let mut s = H::new();
+    let mut m = H::new();
+    for i in 0..n {
+        for j in i + 1..n {
+            let mut u = p[i].0 - p[j].0;
+            let mut v = p[i].1 - p[j].1;
+            if u == 0 {
+                v = 1;
+            } else if v == 0 {
+                u = 1;
+            } else {
+                if u < 0 {
+                    u = -u;
+                    v = -v;
+                }
+                let g = gcd(u.abs(), v.abs());
+                u /= g;
+                v /= g;
+            }
+            *s.entry((u, v)).or_insert(0) += 1;
+            *m.entry((p[i].0 + p[j].0, p[i].1 + p[j].1)).or_insert(0) += 1;
+        }
     }
-    println!("{}", n);
+    println!("{}", calc(&s) - calc(&m));
+}
+
+fn calc(v: &H<(i64, i64), i64>) -> i64 {
+    v.iter().fold(0, |s, (_, c)| s + c * (c - 1) / 2)
 }

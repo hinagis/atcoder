82d86a003678b461a0e1114f65c0790a1de983af
hinagis <hasevent@gmail.com>
2025-08-15 08:26:35 +0000

    e is cleared

--- abc415/src/bin/e.rs
+++ abc415/src/bin/e.rs
@@ -1,12 +1,22 @@
-use proconio::{input as I, fastout as F};
+use itertools::iproduct;
 
-#[F]
 fn main() {
     proconio::input! {
-        n: usize,
+        h: usize,
+        w: usize,
+        a: [[i64; w]; h],
+        p: [i64; h + w - 1]
     }
-    I! {
-        n: usize,
+    let mut dp = vec![vec![i64::MAX; w]; h];
+    dp[h - 1][w - 1] = 0.max(p[h + w - 2] - a[h - 1][w - 1]);
+    for i in (0..h - 1).rev() {
+        dp[i][w - 1] = 0.max(dp[i + 1][w - 1] - a[i][w - 1] + p[i + w - 1]);
     }
-    println!("{}", n);
+    for j in (0..w - 1).rev() {
+        dp[h - 1][j] = 0.max(dp[h - 1][j + 1] - a[h - 1][j] + p[h - 1 + j]);
+    }
+    for (i, j) in iproduct!((0..h - 1).rev(), (0..w - 1).rev()) {
+        dp[i][j] = 0.max(dp[i + 1][j].min(dp[i][j + 1]) - a[i][j] + p[i + j]);
+    }
+    println!("{}", dp[0][0]);
 }

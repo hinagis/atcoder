f444f48e306f51a254e7c33a016a3a35b5240bc4
hinagis <hasevent@gmail.com>
2025-08-15 07:32:35 +0000

    e is cleared

--- abc417/src/bin/e.rs
+++ abc417/src/bin/e.rs
@@ -1,12 +1,45 @@
-use proconio::{input as I, fastout as F};
+use itertools::Itertools;
+use std::collections::VecDeque as Q;
+use proconio::{input as I, fastout as F, marker::Usize1 as U};
 
 #[F]
 fn main() {
-    proconio::input! {
-        n: usize,
+    I! {t: u32}
+    for _ in 0..t {
+        I! {
+            n: usize,
+            m: usize,
+            x: U,
+            y: U
+        }
+        let mut g = vec![vec![]; n];
+        for _ in 0..m {
+            I! {u: U, v: U}
+            g[u].push(v);
+            g[v].push(u);
+        }
+        for i in 0..n {
+            g[i].sort();
+        }
+        let mut f = vec![false; n];
+        let mut p = Q::new();
+        dfs(&g, &mut f, &mut p, y, x);
     }
-    I! {
-        n: usize,
+}
+
+fn dfs(g: &Vec<Vec<usize>>, f: &mut Vec<bool>, p: &mut Q<usize>, y: usize, i: usize) -> bool {
+    if f[i] {return false}
+    f[i] = true;
+    p.push_back(i + 1);
+    for &j in &g[i] {
+        if f[j] {continue}
+        if j == y {
+            p.push_back(y + 1);
+            println!("{}", p.iter().join(" "));
+            return true;
+        }
+        if dfs(g, f, p, y, j) {return true}
     }
-    println!("{}", n);
+    p.pop_back();
+    false
 }

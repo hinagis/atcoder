21f9dd7d1f4d3dc54b529148171476f9f71ece13
hinagis <hasevent@gmail.com>
2025-08-18 09:45:39 +0000

    e is cleared

--- abc419/src/bin/e.rs
+++ abc419/src/bin/e.rs
@@ -5,5 +5,18 @@ fn main() {
         l: usize,
         a: [usize; n]
     }
-    println!("{}", n);
+    const M: usize = 1000000;
+    let mut dp = vec![M; m];
+    dp[0] = 0;
+    for i in 0..l {
+        let mut t = vec![M; m];
+        for j in 0..m {
+            let c = ((i..n).step_by(l)).fold(0, |c, k| c + (m + j - a[k]) % m);
+            for k in 0..m {
+                t[(j + k) % m] = t[(j + k) % m].min(dp[k] + c);
+            }
+        }
+        dp = t;
+    }
+    println!("{}", dp[0]);
 }

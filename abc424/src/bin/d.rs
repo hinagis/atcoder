4fc77fdcf3277a991dfc0c2b2b9a65c5fc2e82be
hinagis <hasevent@gmail.com>
2025-09-24 13:44:46 +0000

    d is cleared

--- abc424/src/bin/d.rs
+++ abc424/src/bin/d.rs
@@ -10,23 +10,30 @@ fn main() {
             w: usize,
             mut s: [C; h]
         }
-        let mut c = 0;
-        for (i, j) in iproduct!(1..h - 1, 1..w - 1) {
-            if s[i][j] != '#' {continue}
-            if (s[i - 1][j] == '#'
-                && ((s[i - 1][j - 1] == '#' && s[i][j - 1] == '#')
-                 || (s[i - 1][j + 1] == '#' && s[i][j + 1] == '#')
-                ))
-               ||(
-               (s[i + 1][j] == '#')
-                && ((s[i + 1][j - 1] == '#' && s[i][j - 1] == '#')
-                 || (s[i + 1][j + 1] == '#' && s[i][j + 1] == '#')
-                ))
-            {
-                s[i][j] = '.';
-                c += 1;
-            }
+        let mut a = vec![vec![true; 1 << w]; 1 << w];
+        for (i, j) in iproduct!(0..1 << w, 0..1 << w) {
+            a[i][j] &= !(0..w - 1)
+                .any(|k| ((i >> k) & 3 == 3) & ((j >> k) & 3 == 3));
         }
-        println!("{}", c);
+
+        let mut dp = vec![50; 1 << w];
+		dp[0] = 0;
+        for i in 0..h {
+			let b = (0..w)
+                .filter(|&j| s[i][j] == '#')
+                .fold(0, |b, j| b | 1 << j);
+            let mut dp2 = vec![50; 1 << w];
+            for j in 0..1 << w {
+				if (j | b) == b {
+                    for k in 0..1 << w {
+						if a[k][j] {
+                            dp2[j] = dp2[j].min(dp[k] + usize::count_ones(j ^ b));
+                        }
+					}
+				}
+			}
+			dp = dp2;
+		}
+        println!("{}", dp.iter().fold(50, |a, &x| a.min(x)));
     }
 }

8d1de3b1f44bc8e47883ab05c184db806a7cccaf
hinagis <hasevent@gmail.com>
2026-03-07 06:23:42 +0000

    d is cleared

--- abc435/src/bin/d.rs
+++ abc435/src/bin/d.rs
@@ -1,10 +1,41 @@
-use proconio::input as I;
-// use proconio::{input as I, fastout as F};
+use proconio::{input as I, fastout as F, marker::Usize1 as U};
 
-// #[F]
+#[F]
 fn main() {
     I! {
         n: usize,
+        m: usize
     }
-    println!("{}", n);
+    let mut t = vec![vec![]; n];
+    let mut f = vec![false; n];
+    for _ in 0..m {
+        I! {
+            x: U,
+            y: U
+        }
+        t[y].push(x);
+    }
+    I! {q: usize}
+    for _ in 0..q {
+        I! {
+            k: u8,
+            v: U
+        }
+        if k == 1 {
+            if f[v] {continue}
+            f[v] = true;
+            let mut b = std::collections::VecDeque::new();
+            b.push_back(v);
+            while let Some(v) = b.pop_front() {
+                for &u in &t[v] {
+                    if !f[u] {
+                        f[u] = true;
+                        b.push_back(u);
+                    }
+                }
+            }
+        } else {
+            println!("{}", if f[v] {"Yes"} else {"No"});
+        }
+    }
 }
